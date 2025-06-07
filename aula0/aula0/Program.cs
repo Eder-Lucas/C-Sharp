bdceebcb6d2d3467505a9fb29cd45b5bdab4d7fd
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-06-07 18:31:14 -0300

    primeiros códigos em c sharp

---
+++ aula0/aula0/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace helloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Dando mais um passo na minha formação dessa vez com o C#! Desejo disciplina, foco e muito aprendizado para mim.");
+        }
+
+    }
+}
