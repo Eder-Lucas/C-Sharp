2a9c909b7f8b019e4e0579c56663c701e508ae84
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-06-09 18:53:06 -0300

    fiz o projeto instanciação na aula02

---
+++ aulas/aula02/instanciacaoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace instanciacaoApp
+{
+    public class Carro //class define os dados que o carro vai ter
+    {
+        public string Modelo;
+        public int Ano;
+    }
+
+    internal class Program //class principal
+    {
+        static void Main()
+        {
+            Carro meuCarro = new Carro(); //istanciacao transforma em objeto
+            meuCarro.Modelo = "Fusção Preto"; //agora pode receber valores
+            meuCarro.Ano = 1980;
+
+            Console.WriteLine("O meu carro dos sonhos é:");
+            Console.WriteLine($"Modelo: {meuCarro.Modelo}, Ano: {meuCarro.Ano}");
+        }
+    }
+}
