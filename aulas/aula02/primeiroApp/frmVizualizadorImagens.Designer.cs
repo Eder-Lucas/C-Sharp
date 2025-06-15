68cc525eeb3dccffd57225ae9c50d322908984aa
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-06-15 17:42:26 -0300

    fiz o "Visualiza Imagens"

---
+++ aulas/aula02/primeiroApp/frmVizualizadorImagens.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace primeiroApp
+{
+    partial class frmVizualizadorImagens
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVizualizadorImagens));
+            pictureBox1 = new PictureBox();
+            pbImagens = new PictureBox();
+            rbStretch = new RadioButton();
+            btnLimpaImagem = new Button();
+            btnMostraImagem = new Button();
+            ofdImagens = new OpenFileDialog();
+            rbNormal = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbImagens).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, -5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(811, 481);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pbImagens
+            // 
+            pbImagens.Image = (Image)resources.GetObject("pbImagens.Image");
+            pbImagens.Location = new Point(34, 22);
+            pbImagens.Name = "pbImagens";
+            pbImagens.Size = new Size(748, 427);
+            pbImagens.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbImagens.TabIndex = 1;
+            pbImagens.TabStop = false;
+            // 
+            // rbStretch
+            // 
+            rbStretch.AutoSize = true;
+            rbStretch.Checked = true;
+            rbStretch.Location = new Point(478, 546);
+            rbStretch.Name = "rbStretch";
+            rbStretch.Size = new Size(76, 24);
+            rbStretch.TabIndex = 3;
+            rbStretch.TabStop = true;
+            rbStretch.Text = "Stretch";
+            rbStretch.UseVisualStyleBackColor = true;
+            rbStretch.CheckedChanged += rbStretch_CheckedChanged;
+            // 
+            // btnLimpaImagem
+            // 
+            btnLimpaImagem.Location = new Point(247, 534);
+            btnLimpaImagem.Name = "btnLimpaImagem";
+            btnLimpaImagem.Size = new Size(166, 49);
+            btnLimpaImagem.TabIndex = 4;
+            btnLimpaImagem.Text = "Limpar imagem";
+            btnLimpaImagem.UseVisualStyleBackColor = true;
+            btnLimpaImagem.Click += btnLimpaImagem_Click;
+            // 
+            // btnMostraImagem
+            // 
+            btnMostraImagem.Location = new Point(34, 534);
+            btnMostraImagem.Name = "btnMostraImagem";
+            btnMostraImagem.Size = new Size(158, 49);
+            btnMostraImagem.TabIndex = 5;
+            btnMostraImagem.Text = "Mostrar imagem";
+            btnMostraImagem.UseVisualStyleBackColor = true;
+            btnMostraImagem.Click += btnMostraImagem_Click;
+            // 
+            // ofdImagens
+            // 
+            ofdImagens.FileName = "nome do arquivo";
+            ofdImagens.Filter = "Images Files (*.PNG;*.JPG;*.GIF)|*.PNG;*.JPG;*.GIF| All Files (*.*)|*.*";
+            ofdImagens.Title = "Selecione a imagem";
+            // 
+            // rbNormal
+            // 
+            rbNormal.AutoSize = true;
+            rbNormal.Location = new Point(594, 546);
+            rbNormal.Name = "rbNormal";
+            rbNormal.Size = new Size(80, 24);
+            rbNormal.TabIndex = 2;
+            rbNormal.Text = "Normal";
+            rbNormal.UseVisualStyleBackColor = true;
+            rbNormal.CheckedChanged += rbNormal_CheckedChanged;
+            // 
+            // frmVizualizadorImagens
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(811, 605);
+            Controls.Add(rbNormal);
+            Controls.Add(btnMostraImagem);
+            Controls.Add(btnLimpaImagem);
+            Controls.Add(rbStretch);
+            Controls.Add(pbImagens);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "frmVizualizadorImagens";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Vizualizador de imagens";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbImagens).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pbImagens;
+        private RadioButton rbStretch;
+        private Button btnLimpaImagem;
+        private Button btnMostraImagem;
+        private OpenFileDialog ofdImagens;
+        private RadioButton rbNormal;
+    }
+}
---
+++ aulas/aula02/primeiroApp/frmVizualizadorImagens.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primeiroApp
+{
+    public partial class frmVizualizadorImagens : Form
+    {
+        public frmVizualizadorImagens()
+        {
+            InitializeComponent();   
+        }
+
+        bool loadImage = false; //Verifica quando a imagem foi carregada
+    
+        //Quando clicar no botao Mostrar Imagem
+        private void btnMostraImagem_Click(object sender, EventArgs e)
+        {
+            //Abre uma janela e se o usuario escolher uma imagem
+            if (ofdImagens.ShowDialog() == DialogResult.OK)
+            {
+                pbImagens.Load(ofdImagens.FileName); //Carrega ela no PictureBox
+                btnMostraImagem.Enabled = false; //Desativa o botao evitando novos envios
+                loadImage = true; //Indica que a imagem foi carregada
+
+                //Torna a visualizacao da imagem automatica sem depender de um novo click no RadioButton
+                if (rbStretch.Checked) //Se o stretch tiver marcado
+                {
+                    pbImagens.SizeMode = PictureBoxSizeMode.StretchImage; //Troca pra o modo Stretch
+                }
+                else                                                    //senao
+                {
+                    pbImagens.SizeMode = PictureBoxSizeMode.Normal; //Troca pra o modo normal
+                }
+            }
+        }
+
+        //Quando clicar no botao Limpar imagem
+        private void btnLimpaImagem_Click(object sender, EventArgs e)
+        {
+            pbImagens.Image = Properties.Resources.drop; //Adiciona a imagem que simboliza que nao ha imagens adicionadas
+            pbImagens.SizeMode = PictureBoxSizeMode.StretchImage; //Forca a troca pro modo stretch
+            btnMostraImagem.Enabled = true; //Ativa o botao para carregar uma nova imagem
+            loadImage = false; //Indica que nao tem uma imagem carregada
+        }
+
+        //Quando o botao radio Stretch for marcado
+        private void rbStretch_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loadImage) //Se tiver imagem carregada
+            {
+                pbImagens.SizeMode = PictureBoxSizeMode.StretchImage; //Troca pra o modo Stretch
+            }
+        }
+
+        //Quando o botao radio Normal for marcado
+        private void rbNormal_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loadImage) //Se tiver imagem carregada
+            {
+                pbImagens.SizeMode = PictureBoxSizeMode.Normal; //Troca pra o modo Normal
+            }
+        }
+    }
+}
