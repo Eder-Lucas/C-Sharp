a87c2b590b1b9674984e45ea69a15f6f01798dc3
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-06-17 12:30:54 -0300

    adicionando comentários no frmPrincipal

--- aulas/aula02/primeiroApp/frmPrincipal.cs
+++ aulas/aula02/primeiroApp/frmPrincipal.cs
@@ -7,6 +7,7 @@ namespace primeiroApp
             InitializeComponent();
         }
 
+        //Botao Mostrar Mensagem
         private void btnMostrarMensagem_Click(object sender, EventArgs e) //evento click
         {
             lblMensagem.Text = "Primeiro Aplicativo em C-Sharp"; //define o texto que vai ser mostrado
@@ -14,18 +15,21 @@ namespace primeiroApp
 
         }
 
-        private void btnPropriedadesCheckBox_Click(object sender, EventArgs e) //ao clicar no botao CheckBox
+        //Botao CheckBox
+        private void btnPropriedadesCheckBox_Click(object sender, EventArgs e)
         {
             frmPropriedadesCheckBox propriedades = new frmPropriedadesCheckBox(); //istanciacao
             propriedades.ShowDialog(); //mostra o formulario CheckBox
         }
 
+        //Botao Imagens
         private void btnImagens_Click(object sender, EventArgs e)
         {
             frmVizualizadorImagens imagens = new frmVizualizadorImagens();
             imagens.ShowDialog();
         }
 
+        //Botao Tabuada
         private void btnTabuada_Click(object sender, EventArgs e)
         {
             frmTabuada tabuada = new frmTabuada();
