7c77982df36806e6fb932b7bcbdefa3ca62ea457
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-06-17 12:29:44 -0300

    adicionando comentários no frmTabuada

--- aulas/aula02/primeiroApp/frmTabuada.cs
+++ aulas/aula02/primeiroApp/frmTabuada.cs
@@ -15,33 +15,37 @@ namespace primeiroApp
         public frmTabuada()
         {
             InitializeComponent();
-            txtTabuada.ReadOnly = true;
+            txtTabuada.ReadOnly = true; //Não permite digitar no campo de tabuada
         }
 
+        //Quando clicar no botão de calcular
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            txtTabuada.Text = "";
+            txtTabuada.Text = ""; //Limpa a tabuada
 
-            if (txtNumero.Text == "")
+            if (txtNumero.Text == "") //Se não tiver número uma mensagem é exibida
             {
                 MessageBox.Show("Você precisa digitar um número para gerar a tabuada",
                                  "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            else
+            else //Se tiver um número o calculo é efetuado
             {
-                double numero, resultado;
-                numero = double.Parse(txtNumero.Text);
+                double numero, resultado; //Permite números decimais
+                numero = double.Parse(txtNumero.Text); //Convertendo o número inserido em Number
 
+                //Loop for 
                 for (int i = 1; i <= 10; i++)
                 {
-                    resultado = numero * i;
-                    txtTabuada.Text += numero + "x" + i + "=" + resultado + "\r\n";
+                    resultado = numero * i; //Vai multiplicando até chegar em 10
+                    txtTabuada.Text += numero + "x" + i + "=" + resultado + "\r\n"; //Exibe no TextBox de forma organizada
                 }
             }
         }
 
+        //Quando clicar no botão limpar
         private void btnLimpar_Click(object sender, EventArgs e)
         {
+            //Todo conteúdo é apagado
             txtNumero.Text = "";
             txtTabuada.Text = "";
         }
