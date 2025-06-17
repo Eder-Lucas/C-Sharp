4b09f9caf5a4f4a8d15d13d4d653a1abd1a6d573
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-06-17 12:20:43 -0300

    fiz o gerador de tabuada

---
+++ aulas/aula02/primeiroApp/frmTabuada.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace primeiroApp
+{
+    partial class frmTabuada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTabuada));
+            label1 = new Label();
+            txtNumero = new TextBox();
+            btnCalcular = new Button();
+            btnLimpar = new Button();
+            txtTabuada = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(308, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Informe qual tabuada deseja calcular:";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(326, 39);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(92, 27);
+            txtNumero.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(24, 96);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(103, 46);
+            btnCalcular.TabIndex = 2;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // btnLimpar
+            // 
+            btnLimpar.Location = new Point(24, 178);
+            btnLimpar.Name = "btnLimpar";
+            btnLimpar.Size = new Size(103, 46);
+            btnLimpar.TabIndex = 3;
+            btnLimpar.Text = "Limpar";
+            btnLimpar.UseVisualStyleBackColor = true;
+            btnLimpar.Click += btnLimpar_Click;
+            // 
+            // txtTabuada
+            // 
+            txtTabuada.Location = new Point(191, 96);
+            txtTabuada.Multiline = true;
+            txtTabuada.Name = "txtTabuada";
+            txtTabuada.Size = new Size(286, 301);
+            txtTabuada.TabIndex = 4;
+            // 
+            // frmTabuada
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(500, 420);
+            Controls.Add(txtTabuada);
+            Controls.Add(btnLimpar);
+            Controls.Add(btnCalcular);
+            Controls.Add(txtNumero);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "frmTabuada";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Tabuada";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNumero;
+        private Button btnCalcular;
+        private Button btnLimpar;
+        private TextBox txtTabuada;
+    }
+}
---
+++ aulas/aula02/primeiroApp/frmTabuada.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primeiroApp
+{
+    public partial class frmTabuada : Form
+    {
+        public frmTabuada()
+        {
+            InitializeComponent();
+            txtTabuada.ReadOnly = true;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            txtTabuada.Text = "";
+
+            if (txtNumero.Text == "")
+            {
+                MessageBox.Show("Você precisa digitar um número para gerar a tabuada",
+                                 "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                double numero, resultado;
+                numero = double.Parse(txtNumero.Text);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    resultado = numero * i;
+                    txtTabuada.Text += numero + "x" + i + "=" + resultado + "\r\n";
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNumero.Text = "";
+            txtTabuada.Text = "";
+        }
+    }
+}
