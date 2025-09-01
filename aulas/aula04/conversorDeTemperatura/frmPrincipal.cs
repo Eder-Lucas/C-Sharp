573c83a99ba786a3aa8a6df10ac19bf8d1ae09a7
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-09-01 20:58:43 -0300

    adicionei a lógica inicial

--- aulas/aula04/conversorDeTemperatura/frmPrincipal.cs
+++ aulas/aula04/conversorDeTemperatura/frmPrincipal.cs
@@ -25,7 +25,62 @@ namespace conversorDeTemperatura
                 {
                     txtResultado.Text = valor + "ºC";
                 }
+            }
+            else if (rbKelvin1.Checked == true)
+            {
+                if (rbCelsius2.Checked == true)
+                {
+                    txtResultado.Text = (valor - 273) + "°C";
+                }
+                else if (rbFahrenheit2.Checked == true)
+                {
+                    txtResultado.Text = (valor * 1.8 - 459.67) + "°F";
+                }
+                else
+                {
+                    txtResultado.Text = valor + "K";
+                }
+            }
+            else if (rbFahrenheit1.Checked == true)
+            {
+                if (rbCelsius2.Checked == true)
+                {
+                    txtResultado.Text = ((valor - 32) / 1.8) + "°C";
+                }
+                else if (rbKelvin2.Checked == true)
+                {
+                    txtResultado.Text = ((valor + 459.67) / 1.8) + "°C";
+                }
+                else
+                {
+                    txtResultado.Text = valor + "°F";
+                }
+            }
+            else
+            {
+                txtResultado.Text = valor.ToString();
+            }
+        }
 
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtValor.Text = "";
+            txtResultado.Clear();
+
+            foreach (Control Componente in gbEntrada.Controls)
+            {
+                if (Componente is RadioButton rb)
+                {
+                    rb.Checked = false;
+                }
+            }
+
+            foreach (Control Componente in gbSaida.Controls)
+            {
+                if (Componente is RadioButton rb)
+                {
+                    rb.Checked = false;
+                }
             }
         }
     }
