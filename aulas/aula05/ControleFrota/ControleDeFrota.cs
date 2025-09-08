d94ccaabdad5489ac06d29977d8a3e27a49ea931
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-09-08 20:01:20 -0300

    usando propriedades na classe

--- aulas/aula05/ControleFrota/ControleDeFrota.cs
+++ aulas/aula05/ControleFrota/ControleDeFrota.cs
@@ -9,11 +9,12 @@ namespace ControleFrota
 
         public class Veiculo
         {
-            private string modelo, placa, km;
-
+            /* se não usasse as propriedades
+            
+            // recebe como parâmetro uma string chamada modelo 
             public void salvaModelo(string modelo)
             {
-                this.modelo = modelo;
+                this.modelo = modelo; //o atributo modelo recebe o valor vindo do parâmetro
             }
 
             public void salvaPlaca(string placa)
@@ -26,9 +27,12 @@ namespace ControleFrota
                 this.km = km;
             }
 
+            //mostrando 
+
+            //metodo que retorna uma string
             public string mostraModelo()
             {
-                return modelo;
+                return modelo; //retorna o valor guardado no metodo 'salvaModelo()'
             }
 
             public string mostraPlaca()
@@ -40,12 +44,41 @@ namespace ControleFrota
             {
                 return km;
             }
+            */
+
+            //usando propriedades
+
+            private string modelo, placa, km;
+
+            public string propriedadeModelo
+            {
+                set { modelo = value; }
+                get { return modelo; }
+            }
+
+            public string propriedadePlaca
+            {
+                set { placa = value; }
+                get { return placa; }
+            }
+
+            public string propriedadeKm
+            {
+                set { km = value; }
+                get { return km; }
+            }
+        }
+
+        //classe filha de veiculos
+        //herda: Modelo, Placa e Km por ser filha
+        public class Caminhao : Veiculo
+        {
+            public string propriedadeEixo { set; get; }
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            Veiculo novoCarro = new Veiculo();
-
+            /* se usasse atributos
             novoCarro.salvaModelo(txtModelo.Text);
             novoCarro.salvaPlaca(txtPlaca.Text);
             novoCarro.salvaKm(txtKm.Text);
@@ -53,6 +86,40 @@ namespace ControleFrota
             txtCarros.Text += novoCarro.mostraModelo() + "\t";
             txtCarros.Text += novoCarro.mostraPlaca() + "\t";
             txtCarros.Text += novoCarro.mostraKm() + "\r\n";
+            */
+
+            if (txtEixo.Visible == false)
+            {
+                Veiculo novoCarro = new Veiculo();
+
+                novoCarro.propriedadeModelo = txtModelo.Text;
+                novoCarro.propriedadePlaca = txtPlaca.Text;
+                novoCarro.propriedadeKm = txtKm.Text;
+
+                txtCarros.AppendText(novoCarro.propriedadeModelo + "\t");
+                txtCarros.AppendText(novoCarro.propriedadePlaca + "\t");
+                txtCarros.AppendText(novoCarro.propriedadeKm + "\r\n");
+            }
+            else
+            {
+                Caminhao novoCaminhao = new Caminhao();
+
+                novoCaminhao.propriedadeModelo = txtModelo.Text;
+                novoCaminhao.propriedadePlaca = txtPlaca.Text;
+                novoCaminhao.propriedadeKm = txtKm.Text;
+                novoCaminhao.propriedadeEixo = txtEixo.Text;
+
+                txtCaminhao.AppendText(novoCaminhao.propriedadeModelo + "\t");
+                txtCaminhao.AppendText(novoCaminhao.propriedadePlaca + "\t");
+                txtCaminhao.AppendText(novoCaminhao.propriedadeKm + "\t");
+                txtCaminhao.AppendText(novoCaminhao.propriedadeEixo + "\r\n");
+            }
+        }
+
+        private void btnCaminhao_Click(object sender, EventArgs e)
+        {
+            lblEixo.Visible = true;
+            txtEixo.Visible = true;
         }
     }
 }
