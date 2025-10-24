bcd060e43d0c158e5439f7ec53a76c52847608b9
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-10-24 10:15:46 -0300

    adicionando mais comentários

--- aulas/aula06/CadastroClientes/frmPrincipal.cs
+++ aulas/aula06/CadastroClientes/frmPrincipal.cs
@@ -28,7 +28,8 @@ namespace CadastroClientes
 
         //quando clicar no botão salvar
         private void btnSalvar_Click(object sender, EventArgs e)
-        {
+        {   
+            //salva os dados da pessoa fisica
             PessoaFisica pF = new()
             {
                 Nome = txtNomePessoaFisica.Text,
@@ -37,6 +38,7 @@ namespace CadastroClientes
                 Rg = txtRG.Text
             };
 
+            //salva os dados da pessoa juridica
             PessoaJuridica pJ = new()
             {
                 Nome = txtEnderecoPessoaJuridica.Text,
@@ -45,9 +47,11 @@ namespace CadastroClientes
                 Ie = txt_IE.Text
             };
 
+            //se o painel de pessoa fisica tiver visivel
             if (painelPessoaFisica.Visible)
             {
-                if (!pF.ValidacaoErro(out string erro))
+                //se tiver erro de validação retorna a função impedindo o fluxo restante do código
+                if (pF.ValidacaoErro(out string erro))
                 {
                     MessageBox.Show(erro,
                         "Erro de validação",
@@ -56,11 +60,15 @@ namespace CadastroClientes
                     return;
                 }
 
+                //mostra os dados no txt caso passe na validação
                 txtPessoaFisica.Text += $"{pF.Nome}\t {pF.Endereco}\t {pF.Cpf}\t {pF.Rg}\r\n";
             }
+
+            //se o painel de pessoa juridica tiver visivel
             else
             {
-                if (!pJ.ValidacaoErro(out string erro))
+                //faz a validação
+                if (pJ.ValidacaoErro(out string erro))
                 {
                     MessageBox.Show(erro,
                         "Erro de validação",
@@ -69,30 +77,40 @@ namespace CadastroClientes
                     return;
                 }
 
+                //mostra os dados no txt se passar na validação
                 txtPessoaJuridica.Text += $"{pJ.Nome}\t {pJ.Endereco}\t {pJ.Cnpj}\t {pJ.Ie}\r\n";
             }
 
+            //ao final limpa todos os txt
             Limpar(this);
         }
 
+        //class abstrata - não pode criar objetos diretamente
         public abstract class Entidade
         {
-            public virtual bool ValidacaoErro(out string erro)
+            //método que será herdado para outras classes
+            public virtual bool ValidacaoErro(out string erro)//out permite retornar dois valores
             {
+                //armazena o tipo do objeto(pessoFisica ou pessoaJuridica) e todas suas propriedades
                 foreach (var prop in this.GetType().GetProperties())
-                {
+                {   
+                    //pega seu valor real e converte para string
+                    //'?' evita erro se o valor for null
                     var valor = prop.GetValue(this)?.ToString();
 
+                    //verifica se o valor é null("") ou espaços vazios(" ")
                     if (string.IsNullOrWhiteSpace(valor))
                     {
+                        //trata o erro
                         string nomeCampo = prop.Name;
-                        erro = $"O campo {nomeCampo} está vazio.";
-                        return false;
+                        erro = $"O campo {nomeCampo} está vazio.";//retorno string
+                        return true;//retorno bool
                     }
                 }
 
+                //se passar na validação
                 erro = "";
-                return true;
+                return false;
             }
         }
 
@@ -134,6 +152,7 @@ namespace CadastroClientes
             public string Ie { set; get; }
         }
 
+        //função para limpar os campos
         public void Limpar(Control parent)
         {
             foreach (Control c in parent.Controls)
