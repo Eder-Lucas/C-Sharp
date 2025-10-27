07176047fd44478f291eb2971d8ffb0e2e33e6f1
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-10-27 14:08:46 -0300

    comentários na class PessoaFisica

--- aulas/aula06/CadastroClientesPolimorfismo/PessoaFisica.cs
+++ aulas/aula06/CadastroClientesPolimorfismo/PessoaFisica.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 namespace CadastroClientesPolimorfismo
 {
+    //filha da class pessoa
     internal class PessoaFisica : Pessoa
     {
+        //atributos
         public string Cpf { set; get; }
         public string Rg { set; get; }
 
+        //regra de validação única para os seus atributos
         public override bool ValidacaoErro(out string erro)
         {
+            //chama a validação da base: Pessoa.ValidacaoErro
             if (base.ValidacaoErro(out erro)) return true;
 
+            //validação dessa class
             if (string.IsNullOrWhiteSpace(Cpf))
             {
                 erro = "O campo CPF não foi preenchido!";
@@ -31,9 +36,14 @@ namespace CadastroClientesPolimorfismo
             return false;
         }
 
+        //mostra seus dados
         public override void MostrarDados(TextBox txt)
         {
+            //chama Pessoa.MostrarDados
+            //carregando os dados anteriores
             base.MostrarDados(txt);
+
+            //mostra os dados dessa class
             txt.AppendText($"{Cpf}\t {Rg}\r\n");
         }
 
