e55944d191513c1e5899af35975a68e20674057a
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-10-27 14:08:27 -0300

    melhorei a validação com Regex

--- aulas/aula06/CadastroClientesPolimorfismo/Pessoa.cs
+++ aulas/aula06/CadastroClientesPolimorfismo/Pessoa.cs
@@ -2,45 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CadastroClientesPolimorfismo
 {
+    //filha da class entidade
     internal class Pessoa : Entidade
     {
+        //atributos especificos
         public string Nome { get; set; }
         public string Endereco { get; set; }
 
+        //metódo de validação por polimorfismo
         public override bool ValidacaoErro(out string erro)
         {
-            if (double.TryParse(Nome, out double _))
-            {
-                erro = $"'{Nome}' é um valor inválido, o campo Nome precisa conter letras.";
-                return true;
-            }
-            
-            if (double.TryParse(Endereco, out double _))
-            {
-                erro = $"'{Endereco}' é um valor inválido, o campo Endereço precisa conter letras.";
-                return true;
-            }
-
+            //verifica se tem campos vazios
             if (string.IsNullOrWhiteSpace(Nome))
             {
                 erro = "O campo Nome não foi preenchido!";
                 return true;
-            }
-
-            if (string.IsNullOrWhiteSpace(Endereco))
+            } else if (string.IsNullOrWhiteSpace(Endereco))
             {
                 erro = "O campo Endereço não foi preenchido!";
                 return true;
             }
 
+            //testando regex para validação
+            string regexNome = @"^[\p{L} ]+$";
+            string regexEndereco = @"^[\p{L}0-9 ]+, [\p{L}0-9 \-]+$";
+
+            //valida o dado enviado com regex
+            if (!Regex.IsMatch(Nome, regexNome))
+            {
+                erro = $"O campo Nome não foi preenchido corretamente.";
+                return true;
+            }
+
+            if (!Regex.IsMatch(Endereco, regexEndereco))
+            {
+                erro = $"O campo Endereço não foi preenchido corretamente.";
+                return true;
+            }
+
+            //caso não possua erros
             erro = "";
             return false;
         }
 
+        //mostra os dados por polimorfismo
         public override void MostrarDados(TextBox txt)
         {
             txt.AppendText($"{Nome}\t {Endereco}\t");
