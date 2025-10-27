562a470142792a2670c56cdde6c8075e9052d771
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-10-27 14:08:07 -0300

    adicionei comentários e alguns ajustes

--- aulas/aula06/CadastroClientesPolimorfismo/frmPrincipal.cs
+++ aulas/aula06/CadastroClientesPolimorfismo/frmPrincipal.cs
@@ -1,5 +1,8 @@
 using System.Configuration;
+using System.Diagnostics;
 using System.Drawing;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
 
 namespace CadastroClientesPolimorfismo
 {
@@ -29,10 +32,15 @@ namespace CadastroClientesPolimorfismo
         //quando clicar no botão salvar
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            //usando polimorfismo
+            //Entidade é a class pai | pessoa é a variável
+            //a variável pode referenciar qualquer objeto que seja do tipo Entidade ou herde dela
             Entidade pessoa;
 
+            //se o painel pessoa fisica tiver visivel
             if (painelPessoaFisica.Visible)
             {
+                //usando a variável para criar um objeto novo do tipo pessoa fisica
                 pessoa = new PessoaFisica
                 {
                     Nome = txtNomePessoaFisica.Text,
@@ -41,8 +49,11 @@ namespace CadastroClientesPolimorfismo
                     Rg = txtRG.Text
                 };
             }
+
+            //se o painel pessoa juridica tiver visivel
             else
             {
+                //usa a variável para criar um novo objeto do tipo pessoa juridica
                 pessoa = new PessoaJuridica
                 {
                     Nome = txtNomePessoaJuridica.Text,
@@ -50,9 +61,9 @@ namespace CadastroClientesPolimorfismo
                     Cnpj = txtCNPJ.Text,
                     Ie = txt_IE.Text
                 };
-
             }
 
+            //chama a validação da variável entidade
             if (pessoa.ValidacaoErro(out string erro))
             {
                 MessageBox.Show(erro, "Erro de validação",
@@ -61,6 +72,27 @@ namespace CadastroClientesPolimorfismo
                 return;
             }
 
+            //chama o metódo para mostrar os dados no txt
+            switch (pessoa)
+            {
+                //caso pessoa for referênciado como fisica ou juridica
+                case PessoaFisica pf:                    
+                    pf.MostrarDados(txtPessoaFisica);
+                    break;
+
+                case PessoaJuridica pj:
+                    pj.MostrarDados(txtPessoaJuridica);
+                    break;
+
+                //caso tenha um tipo desconchecido exibe um erro
+                default:
+                    MessageBox.Show("Tipo de pessoa desconhecido!", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+
+            /*
+            --- OUTRA FORMA DE MOSTRAR OS DADOS ---
             if (pessoa is PessoaFisica)
             {
                 pessoa.MostrarDados(txtPessoaFisica);
@@ -69,6 +101,7 @@ namespace CadastroClientesPolimorfismo
             {
                 pessoa.MostrarDados(txtPessoaJuridica);
             }
+            */
 
             //ao final limpa todos os txt
             Limpar(this);
