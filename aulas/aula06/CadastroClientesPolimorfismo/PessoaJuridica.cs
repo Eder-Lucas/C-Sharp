10014e338ef04d90f1164dc65a1357b351593e3d
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-10-27 14:09:03 -0300

    Comentários na class PessoaJuridica

--- aulas/aula06/CadastroClientesPolimorfismo/PessoaJuridica.cs
+++ aulas/aula06/CadastroClientesPolimorfismo/PessoaJuridica.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 namespace CadastroClientesPolimorfismo
 {
+    //filha da class pessoa
     internal class PessoaJuridica : Pessoa
     {
+        //atributos
         public string Cnpj { set; get; }
         public string Ie { set; get; }
 
+        //validação dos campos
         public override bool ValidacaoErro(out string erro)
         {
+            //chama a validação da base: Pessoa.validacaoErro
             if (base.ValidacaoErro(out erro)) return true;
 
+            //validação dessa class
             if (string.IsNullOrWhiteSpace(Cnpj))
             {
                 erro = "O campo CNPJ deve ser preenchido!";
@@ -31,9 +36,13 @@ namespace CadastroClientesPolimorfismo
             return false;
         }
 
+        //metódo para mostrar os dados no txt
         public override void MostrarDados(TextBox txt)
         {
+            //carrega dos dados da class pai Pessoa
             base.MostrarDados(txt);
+
+            //carrega os dados de pessoa juridica
             txt.AppendText($"{Cnpj}\t {Ie}\r\n");
         }
     }
