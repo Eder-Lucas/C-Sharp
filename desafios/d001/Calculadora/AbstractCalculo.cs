40230f832bc4c3d8ca1304d3368f6ddb632b0787
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-12-23 19:54:33 -0300

    implementando classes

---
+++ desafios/d001/Calculadora/AbstractCalculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    public abstract class AbstractCalculo
+    {
+        //propriedades abstratas
+        public abstract decimal valorVisor { set; get; }
+        public abstract decimal valorAnterior { set; get; }
+        public abstract decimal valorResultado { set; get; }
+        public abstract string operacao { set; get; }
+
+        //Método abstrato do tipo decimal
+        public abstract decimal Calculo();
+    }
+}
---
+++ desafios/d001/Calculadora/ObjetoCalculo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    //classe ObjetoCalculo é filha da classe AbstractCalculo
+    //herdando suas caracteristicas
+    public class ObjetoCalculo : AbstractCalculo
+    {
+        //propriedades referentes aos cálculos, herdadas da classe AbstractCalculo
+        public override decimal valorVisor { get; set; }
+        public override decimal valorAnterior { get; set; }
+        public override decimal valorResultado { get; set; }
+        public override string operacao { get; set; }
+
+        //método para realizar as operações, também herdado da classe pai
+        public override decimal Calculo()
+        {
+            switch (operacao)
+            {
+                case " + ":
+                    valorResultado = valorAnterior + valorVisor;
+                    break;
+
+                case " - ":
+                    valorResultado = valorAnterior - valorVisor;
+                    break;
+
+                case " x ":
+                    valorResultado = valorAnterior * valorVisor;
+                    break;
+
+                case " ÷ ":
+                    valorResultado = valorAnterior / valorVisor;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return Math.Round(valorResultado, 2);
+        }
+    }
+}
