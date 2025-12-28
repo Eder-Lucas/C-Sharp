72c675d45602cb353ecf3371bf6c31eb07cef044
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-12-28 00:00:03 -0300

    iniciando a aula08

---
+++ aulas/aula08/Consultorio/frmPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consultorio
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void consultasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.consultasBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.consultasDataSet);
+
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'consultasDataSet.Consultas'. Você pode movê-la ou removê-la conforme necessário.
+            this.consultasTableAdapter.Fill(this.consultasDataSet.Consultas);
+
+        }
+    }
+}
