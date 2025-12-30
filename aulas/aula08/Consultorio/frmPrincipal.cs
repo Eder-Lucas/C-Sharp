6f853036588c550e99a3e5d4523d023a19119c28
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-12-30 12:59:01 -0300

    lógica do botão Pesquisar

--- aulas/aula08/Consultorio/frmPrincipal.cs
+++ aulas/aula08/Consultorio/frmPrincipal.cs
@@ -34,5 +34,22 @@ namespace Consultorio
             this.consultasTableAdapter.Fill(this.consultasDataSet.Consultas);
 
         }
+
+        // Ao clicar no botão Pesquisar
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            // Verifica qual RadioButton está selecionado e chama o método com a query adequada
+            if (rbMedico.Checked)
+            {
+                // Chama o método para pesquisar por médico passando o texto do TextBox como parâmetro
+                // O resultado da consulta é mostrado no DataGridView
+                dtgPesquisa.DataSource = consultasTableAdapter.RetornarMedico(txtPesquisa.Text);
+            }
+            else
+            {
+                // Chama o método para pesquisar por paciente
+                dtgPesquisa.DataSource = consultasTableAdapter.RetornarPaciente(txtPesquisa.Text);
+            }
+        }
     }
 }
