3c3910ea6fc4ec5f1ea40cdd05333ba1717454e2
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-12-30 18:00:49 -0300

    modificação no botão Salvar
    - agora ele atualiza o banco assim que o botão Salvar recebe um click, o que modifica a forma de exibição do DataGridView

--- atividades-extras/atividade01/ControleDeDespesas/frmPrincipal.cs
+++ atividades-extras/atividade01/ControleDeDespesas/frmPrincipal.cs
@@ -17,12 +17,15 @@ namespace ControleDeDespesas
             InitializeComponent();
         }
 
+        //botão salvar
         private void contasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
             this.Validate();
             this.contasBindingSource.EndEdit();
             this.tableAdapterManager.UpdateAll(this.despesasDataSet);
 
+            //atualiza a exibição dos dados na tela
+            this.contasTableAdapter.Fill(this.despesasDataSet.Contas);
         }
 
         private void frmPrincipal_Load(object sender, EventArgs e)
