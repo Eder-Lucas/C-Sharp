aaf58dc285da08716f67210e577cf8858943b947
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-01-04 18:48:03 -0300

    adicionei duas consultas ao banco

--- aulas/aula09/Escola/EscolaDataSet.Designer.cs
+++ aulas/aula09/Escola/EscolaDataSet.Designer.cs
@@ -914,11 +914,23 @@ SELECT ID_ALUNO, NOME, TELEFONE, EMAIL, CURSO, NASCIMENTO FROM Alunos WHERE (ID_
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[3];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT ID_ALUNO, NOME, TELEFONE, EMAIL, CURSO, NASCIMENTO FROM dbo.Alunos";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT ID_ALUNO, NOME, TELEFONE, EMAIL, CURSO, NASCIMENTO\r\nFROM     Alunos\r\nWHERE" +
+                "  (NOME = @nomeAluno)\r\nORDER BY ID_ALUNO";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@nomeAluno", global::System.Data.SqlDbType.NVarChar, 50, global::System.Data.ParameterDirection.Input, 0, 0, "NOME", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[2] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[2].Connection = this.Connection;
+            this._commandCollection[2].CommandText = "SELECT ID_ALUNO, NOME, TELEFONE, EMAIL, CURSO, NASCIMENTO\r\nFROM     Alunos\r\nWHERE" +
+                "  (CURSO = @nomeCurso)\r\nORDER BY ID_ALUNO";
+            this._commandCollection[2].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@nomeCurso", global::System.Data.SqlDbType.NVarChar, 50, global::System.Data.ParameterDirection.Input, 0, 0, "CURSO", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -940,6 +952,40 @@ SELECT ID_ALUNO, NOME, TELEFONE, EMAIL, CURSO, NASCIMENTO FROM Alunos WHERE (ID_
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual EscolaDataSet.AlunosDataTable GetData() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            EscolaDataSet.AlunosDataTable dataTable = new EscolaDataSet.AlunosDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual EscolaDataSet.AlunosDataTable RetornarAluno(string nomeAluno) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((nomeAluno == null)) {
+                throw new global::System.ArgumentNullException("nomeAluno");
+            }
+            else {
+                this.Adapter.SelectCommand.Parameters[0].Value = ((string)(nomeAluno));
+            }
+            EscolaDataSet.AlunosDataTable dataTable = new EscolaDataSet.AlunosDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual EscolaDataSet.AlunosDataTable RetornarCurso(string nomeCurso) {
+            this.Adapter.SelectCommand = this.CommandCollection[2];
+            if ((nomeCurso == null)) {
+                throw new global::System.ArgumentNullException("nomeCurso");
+            }
+            else {
+                this.Adapter.SelectCommand.Parameters[0].Value = ((string)(nomeCurso));
+            }
             EscolaDataSet.AlunosDataTable dataTable = new EscolaDataSet.AlunosDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
