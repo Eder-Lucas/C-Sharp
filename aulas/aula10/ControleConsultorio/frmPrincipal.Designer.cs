f3b877d8d8b316b27d3f2ed61f59e90807cd43e6
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-01-09 16:58:32 -0300

    adicionando os panel e botões
    - panels onde serão carregados os UserControls
    
    - botões para controlar as exibições

--- aulas/aula10/ControleConsultorio/frmPrincipal.Designer.cs
+++ aulas/aula10/ControleConsultorio/frmPrincipal.Designer.cs
@@ -30,10 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.pnlConteudo = new System.Windows.Forms.Panel();
             this.btnMedicos = new System.Windows.Forms.ToolStripButton();
             this.btnPacientes = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnFechar = new System.Windows.Forms.ToolStripButton();
             this.btnConsultas = new System.Windows.Forms.ToolStripButton();
+            this.btnPesquisas = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,12 +47,28 @@
             this.btnMedicos,
             this.btnPacientes,
             this.toolStripSeparator1,
-            this.btnConsultas});
+            this.btnConsultas,
+            this.btnPesquisas,
+            this.btnFechar});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(815, 59);
+            this.toolStrip1.Size = new System.Drawing.Size(805, 59);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 59);
+            // 
+            // pnlConteudo
+            // 
+            this.pnlConteudo.BackColor = System.Drawing.Color.Transparent;
+            this.pnlConteudo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlConteudo.Location = new System.Drawing.Point(0, 59);
+            this.pnlConteudo.Name = "pnlConteudo";
+            this.pnlConteudo.Size = new System.Drawing.Size(805, 473);
+            this.pnlConteudo.TabIndex = 2;
             // 
             // btnMedicos
             // 
@@ -73,10 +92,17 @@
             this.btnPacientes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnPacientes.Click += new System.EventHandler(this.btnPacientes_Click);
             // 
-            // toolStripSeparator1
+            // btnFechar
             // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 59);
+            this.btnFechar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnFechar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnFechar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(56, 56);
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnFechar.Visible = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
             // btnConsultas
             // 
@@ -89,13 +115,23 @@
             this.btnConsultas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnConsultas.Click += new System.EventHandler(this.btnConsultas_Click);
             // 
+            // btnPesquisas
+            // 
+            this.btnPesquisas.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnPesquisas.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPesquisas.Name = "btnPesquisas";
+            this.btnPesquisas.Size = new System.Drawing.Size(75, 56);
+            this.btnPesquisas.Text = "Pesquisas";
+            this.btnPesquisas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnPesquisas.Click += new System.EventHandler(this.btnPesquisas_Click);
+            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::ControleConsultorio.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(815, 532);
+            this.ClientSize = new System.Drawing.Size(805, 532);
+            this.Controls.Add(this.pnlConteudo);
             this.Controls.Add(this.toolStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -103,6 +139,7 @@
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Controle para Consultório";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -114,9 +151,12 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton btnMedicos;
-        private System.Windows.Forms.ToolStripButton btnConsultas;
+        private System.Windows.Forms.ToolStripButton btnFechar;
         private System.Windows.Forms.ToolStripButton btnPacientes;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Panel pnlConteudo;
+        private System.Windows.Forms.ToolStripButton btnConsultas;
+        private System.Windows.Forms.ToolStripButton btnPesquisas;
     }
 }
 
