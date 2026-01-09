3ee90237a33db19e31c608df0cab632abb9d5280
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-01-09 16:54:27 -0300

    Criei o UserControl da logo

---
+++ aulas/aula10/ControleConsultorio/ucLogo.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ControleConsultorio
+{
+    partial class ucLogo
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ucLogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ControleConsultorio.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Name = "ucLogo";
+            this.Size = new System.Drawing.Size(823, 521);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
