2bc0bd356c13d2feba230906dce2e9449551949e
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-01-10 22:41:11 -0300

    criei uma query e um dataGridView

--- aulas/aula10/ControleConsultorio/ConsultasDataSet.Designer.cs
+++ aulas/aula10/ControleConsultorio/ConsultasDataSet.Designer.cs
@@ -1979,12 +1979,23 @@ SELECT ID_CONSULTA, ID_MEDICO, ID_PACIENTE, DATA, HORARIO, RETORNO FROM Consulta
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT ID_CONSULTA, ID_MEDICO, ID_PACIENTE, DATA, HORARIO, RETORNO FROM dbo.Consu" +
                 "ltas";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = @"SELECT Paciente.NOME_PACIENTE, Medico.NOME_MEDICO, Consultas.ID_CONSULTA, Consultas.ID_MEDICO, Consultas.ID_PACIENTE, Consultas.DATA, Consultas.HORARIO, Consultas.RETORNO
+FROM     Consultas INNER JOIN
+                  Medico ON Consultas.ID_MEDICO = Medico.ID_MEDICO INNER JOIN
+                  Paciente ON Consultas.ID_PACIENTE = Paciente.ID_PACIENTE
+WHERE  (Consultas.DATA >= @dataInicio AND Consultas.DATA < DATEADD(DAY, 1, @dataFinal))
+ORDER BY Consultas.DATA";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@dataInicio", global::System.Data.SqlDbType.DateTime, 8, global::System.Data.ParameterDirection.Input, 0, 0, "DATA", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@dataFinal", global::System.Data.SqlDbType.DateTime, 8, global::System.Data.ParameterDirection.Input, 0, 0, "DATA", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -2006,6 +2017,19 @@ SELECT ID_CONSULTA, ID_MEDICO, ID_PACIENTE, DATA, HORARIO, RETORNO FROM Consulta
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual ConsultasDataSet.ConsultasDataTable GetData() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            ConsultasDataSet.ConsultasDataTable dataTable = new ConsultasDataSet.ConsultasDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual ConsultasDataSet.ConsultasDataTable retornarConsultas(System.DateTime dataInicio, System.DateTime dataFinal) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            this.Adapter.SelectCommand.Parameters[0].Value = ((System.DateTime)(dataInicio));
+            this.Adapter.SelectCommand.Parameters[1].Value = ((System.DateTime)(dataFinal));
             ConsultasDataSet.ConsultasDataTable dataTable = new ConsultasDataSet.ConsultasDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
