e00fea09e3ceceb233c6802a4f61947950de1d25
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-01-11 22:24:25 -0300

    finalizei a lógica do btnPesquisa do UserControl

--- aulas/aula10/ControleConsultorio/ucPesquisa.cs
+++ aulas/aula10/ControleConsultorio/ucPesquisa.cs
@@ -19,7 +19,31 @@ namespace ControleConsultorio
 
         private void btnPesquisaUC_Click(object sender, EventArgs e)
         {
-            dtgConsultas.DataSource = consultasTableAdapter1.retornarConsultas(dtpInicial.Value, dtpFinal.Value);
+            // No banco de dados foi salvo com horas, pois é um datetime
+            // Pega somente a data (sem hora)
+            // As horas interferem na query
+            DateTime dataInicio = dtpInicial.Value.Date;
+            DateTime dataFinal = dtpFinal.Value.Date;
+
+            // Verifica se o campo de pesquisa está vazio
+            // Se tiver faz a pesquisa somente por data
+            if (String.IsNullOrEmpty(txtPesquisa.Text))
+            {
+                dtgConsultas.DataSource = consultasTableAdapter1.retornarConsultas(dataInicio, dataFinal); 
+            }
+            // Se o campo de pesquisa tiver algo digitado
+            else
+            {
+                // Faz a consulta (query) conforme o radio button selecionado
+                if (rbPaciente.Checked)
+                {
+                    dtgConsultas.DataSource = consultasTableAdapter1.retornarPaciente(txtPesquisa.Text, dataInicio, dataFinal);
+                }
+                else if (rbMedico.Checked)
+                {
+                    dtgConsultas.DataSource = consultasTableAdapter1.retornarMedico(txtPesquisa.Text, dataInicio, dataFinal);
+                }
+            }
         }
     }
 }
