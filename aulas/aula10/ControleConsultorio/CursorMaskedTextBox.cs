75967f7f498a32211f3ab0dfa7f3b37976e87c1c
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-01-13 19:44:23 -0300

    criei uma classe estática
    - classe responsável por ajustar o cursor dos txt numéricos que possuem MaskedTextBox

---
+++ aulas/aula10/ControleConsultorio/CursorMaskedTextBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleConsultorio
+{
+    internal static class CursorMaskedTextBox
+    {
+        public static void AdicionaEvento(MaskedTextBox mtb)
+        {
+            if (mtb == null) return;
+
+            mtb.Enter -= MaskedTextBox_Enter;
+            mtb.Enter += MaskedTextBox_Enter;
+        }
+
+        public static void MaskedTextBox_Enter(object sender, EventArgs e)
+        {
+            if (!(sender is MaskedTextBox mtb)) return;
+
+            mtb.BeginInvoke((MethodInvoker)(() =>
+            {
+                if (mtb.MaskedTextProvider.AssignedEditPositionCount == 0)
+                {
+                    int pos = mtb.MaskedTextProvider.FindEditPositionFrom(0, true);
+
+                    mtb.SelectionStart = pos;
+                    mtb.SelectionLength = 0;
+                }
+                else
+                {
+                    mtb.SelectAll();
+                }
+            }));
+        }
+
+        public static void AjustaCursor(Control parent)
+        {
+            foreach (Control ctl in parent.Controls)
+            {
+                if (ctl is MaskedTextBox mtb)
+                {
+                    AdicionaEvento(mtb);
+                }
+
+                if (ctl.HasChildren)
+                {
+                    AjustaCursor(ctl);
+                }
+            }
+        }
+     }
+}
