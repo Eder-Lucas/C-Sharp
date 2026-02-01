091ce5fd8851553d6efa55504a3f264167f46229
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-01 16:21:05 -0300

    alteração no banco de dados
    - criei novas consultas
    - corrigi alguns erros

--- desafios/d002/Pizzaria/PizzariaDataSet.Designer.cs
+++ desafios/d002/Pizzaria/PizzariaDataSet.Designer.cs
@@ -1241,10 +1241,10 @@ namespace Pizzaria {
             
             [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
             [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
-            public SaborRow AddSaborRow(int ID_SABOR, string NOME_SABOR, string INGREDIENTES) {
+            public SaborRow AddSaborRow(string NOME_SABOR, string INGREDIENTES) {
                 SaborRow rowSaborRow = ((SaborRow)(this.NewRow()));
                 object[] columnValuesArray = new object[] {
-                        ID_SABOR,
+                        null,
                         NOME_SABOR,
                         INGREDIENTES};
                 rowSaborRow.ItemArray = columnValuesArray;
@@ -1292,6 +1292,9 @@ namespace Pizzaria {
                 base.Columns.Add(this.columnINGREDIENTES);
                 this.Constraints.Add(new global::System.Data.UniqueConstraint("Constraint1", new global::System.Data.DataColumn[] {
                                 this.columnID_SABOR}, true));
+                this.columnID_SABOR.AutoIncrement = true;
+                this.columnID_SABOR.AutoIncrementSeed = -1;
+                this.columnID_SABOR.AutoIncrementStep = -1;
                 this.columnID_SABOR.AllowDBNull = false;
                 this.columnID_SABOR.Unique = true;
                 this.columnNOME_SABOR.AllowDBNull = false;
@@ -3314,11 +3317,41 @@ SELECT ID_SABOR, NOME_SABOR, INGREDIENTES FROM Sabor WHERE (ID_SABOR = @ID_SABOR
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[6];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT ID_SABOR, NOME_SABOR, INGREDIENTES FROM dbo.Sabor";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "UPDATE  Sabor\r\nSET          NOME_SABOR = @NOME_SABOR, INGREDIENTES = @INGREDIENTE" +
+                "S\r\nWHERE  (ID_SABOR = @Original_ID_SABOR); ";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@NOME_SABOR", global::System.Data.SqlDbType.NVarChar, 50, global::System.Data.ParameterDirection.Input, 0, 0, "NOME_SABOR", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@INGREDIENTES", global::System.Data.SqlDbType.NVarChar, 255, global::System.Data.ParameterDirection.Input, 0, 0, "INGREDIENTES", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Original_ID_SABOR", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "ID_SABOR", global::System.Data.DataRowVersion.Original, false, null, "", "", ""));
+            this._commandCollection[2] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[2].Connection = this.Connection;
+            this._commandCollection[2].CommandText = "DELETE FROM Sabor\r\nWHERE  (ID_SABOR = @Original_ID_SABOR)";
+            this._commandCollection[2].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Original_ID_SABOR", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "ID_SABOR", global::System.Data.DataRowVersion.Original, false, null, "", "", ""));
+            this._commandCollection[3] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[3].Connection = this.Connection;
+            this._commandCollection[3].CommandText = "SELECT ID_SABOR, NOME_SABOR, INGREDIENTES\r\nFROM     Sabor\r\nWHERE  (NOME_SABOR = @" +
+                "NomeSabor)";
+            this._commandCollection[3].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[3].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@NomeSabor", global::System.Data.SqlDbType.NVarChar, 50, global::System.Data.ParameterDirection.Input, 0, 0, "NOME_SABOR", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[4] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[4].Connection = this.Connection;
+            this._commandCollection[4].CommandText = "SELECT ID_SABOR, NOME_SABOR, INGREDIENTES FROM dbo.Sabor";
+            this._commandCollection[4].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[5] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[5].Connection = this.Connection;
+            this._commandCollection[5].CommandText = "INSERT INTO Sabor\r\n                  (NOME_SABOR, INGREDIENTES)\r\nVALUES (@NOME_SA" +
+                "BOR,@INGREDIENTES); ";
+            this._commandCollection[5].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[5].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@NOME_SABOR", global::System.Data.SqlDbType.NVarChar, 50, global::System.Data.ParameterDirection.Input, 0, 0, "NOME_SABOR", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[5].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@INGREDIENTES", global::System.Data.SqlDbType.NVarChar, 255, global::System.Data.ParameterDirection.Input, 0, 0, "INGREDIENTES", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -3340,6 +3373,34 @@ SELECT ID_SABOR, NOME_SABOR, INGREDIENTES FROM Sabor WHERE (ID_SABOR = @ID_SABOR
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual PizzariaDataSet.SaborDataTable GetData() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            PizzariaDataSet.SaborDataTable dataTable = new PizzariaDataSet.SaborDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual PizzariaDataSet.SaborDataTable PesquisaNomeSabor(string NomeSabor) {
+            this.Adapter.SelectCommand = this.CommandCollection[3];
+            if ((NomeSabor == null)) {
+                throw new global::System.ArgumentNullException("NomeSabor");
+            }
+            else {
+                this.Adapter.SelectCommand.Parameters[0].Value = ((string)(NomeSabor));
+            }
+            PizzariaDataSet.SaborDataTable dataTable = new PizzariaDataSet.SaborDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual PizzariaDataSet.SaborDataTable RetornarSabores() {
+            this.Adapter.SelectCommand = this.CommandCollection[4];
             PizzariaDataSet.SaborDataTable dataTable = new PizzariaDataSet.SaborDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
@@ -3495,6 +3556,101 @@ SELECT ID_SABOR, NOME_SABOR, INGREDIENTES FROM Sabor WHERE (ID_SABOR = @ID_SABOR
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Update, true)]
         public virtual int Update(string NOME_SABOR, string INGREDIENTES, int Original_ID_SABOR, string Original_NOME_SABOR, string Original_INGREDIENTES) {
             return this.Update(Original_ID_SABOR, NOME_SABOR, INGREDIENTES, Original_ID_SABOR, Original_NOME_SABOR, Original_INGREDIENTES);
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Update, false)]
+        public virtual int Alterar(string NOME_SABOR, string INGREDIENTES, int Original_ID_SABOR) {
+            global::System.Data.SqlClient.SqlCommand command = this.CommandCollection[1];
+            if ((NOME_SABOR == null)) {
+                throw new global::System.ArgumentNullException("NOME_SABOR");
+            }
+            else {
+                command.Parameters[0].Value = ((string)(NOME_SABOR));
+            }
+            if ((INGREDIENTES == null)) {
+                throw new global::System.ArgumentNullException("INGREDIENTES");
+            }
+            else {
+                command.Parameters[1].Value = ((string)(INGREDIENTES));
+            }
+            command.Parameters[2].Value = ((int)(Original_ID_SABOR));
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Delete, false)]
+        public virtual int Excluir(int Original_ID_SABOR) {
+            global::System.Data.SqlClient.SqlCommand command = this.CommandCollection[2];
+            command.Parameters[0].Value = ((int)(Original_ID_SABOR));
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "18.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Insert, false)]
+        public virtual int Salvar(string NOME_SABOR, string INGREDIENTES) {
+            global::System.Data.SqlClient.SqlCommand command = this.CommandCollection[5];
+            if ((NOME_SABOR == null)) {
+                throw new global::System.ArgumentNullException("NOME_SABOR");
+            }
+            else {
+                command.Parameters[0].Value = ((string)(NOME_SABOR));
+            }
+            if ((INGREDIENTES == null)) {
+                throw new global::System.ArgumentNullException("INGREDIENTES");
+            }
+            else {
+                command.Parameters[1].Value = ((string)(INGREDIENTES));
+            }
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            return returnValue;
         }
     }
     
