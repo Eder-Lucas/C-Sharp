06e50518a9047dd8fcd98491a999fb9e6a7aa6ab
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-08 18:39:34 -0300

    adicionando comentários

--- desafios/d002/Pizzaria/frmPizzas.cs
+++ desafios/d002/Pizzaria/frmPizzas.cs
@@ -32,15 +32,15 @@ namespace Pizzaria
             txtIngrediente.Clear();
         }
 
-        //Quando o formulário carregar, listar os sabores
-        private void frmPizzas_Load(object sender, EventArgs e)
+        private void btnNovo_Click(object sender, EventArgs e)
         {
-            ListarSabores();
+            Limpar();
         }
 
         //Ao clicar no botão salvar
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            //Verifica se o código é igual a zero, se sim salva um novo sabor
             if (txtCodigo.Text == "0")
             {
                 //Executa a query de salvar
@@ -56,8 +56,10 @@ namespace Pizzaria
                 ListarSabores();
                 Limpar(); 
             }
+            //Caso contrário, se for diferente de zero altera o sabor
             else
             {
+                //Executa a query de alteração
                 saborTableAdapter1.Alterar(txtNome.Text, txtIngrediente.Text, Convert.ToInt32(txtCodigo.Text));
 
                 //Exibe a mensagem de sucesso
@@ -72,30 +74,11 @@ namespace Pizzaria
             }
         }
 
-        private void btnNovo_Click(object sender, EventArgs e)
-        {
-            Limpar();
-        }
-
-        //Ao clicar no botão de pesquisar
-        private void btnPesquisa_Click(object sender, EventArgs e)
-        {
-            //Executa a query de pesquisa pelo nome
-            dtgSabores.DataSource = saborTableAdapter1.PesquisaNomeSabor(txtPesquisa.Text);
-
-            //Limpa os campos
-            Limpar();
-        }
-
-        private void dtgSabores_DoubleClick(object sender, EventArgs e)
-        {
-            txtCodigo.Text = dtgSabores.Rows[dtgSabores.CurrentRow.Index].Cells["CODIGO"].Value.ToString();
-            txtNome.Text = dtgSabores.Rows[dtgSabores.CurrentRow.Index].Cells["NOME"].Value.ToString();
-            txtIngrediente.Text = dtgSabores.Rows[dtgSabores.CurrentRow.Index].Cells["INGREDIENTES"].Value.ToString();
-        }
-
+        //Ao clicar no botão excluir
         private void btnExcluir_Click(object sender, EventArgs e)
         {
+            //Verifica se o código é diferente de zero
+            //Se sim, significa que há um sabor selecionado podendo excluí-lo
             if (txtCodigo.Text != "0")
             {
                 //Executa a query de exclusão
@@ -111,6 +94,31 @@ namespace Pizzaria
                 ListarSabores();
                 Limpar();
             }
+        }
+
+        //Ao clicar no botão de pesquisar
+        private void btnPesquisa_Click(object sender, EventArgs e)
+        {
+            //Executa a query de pesquisa pelo nome
+            dtgSabores.DataSource = saborTableAdapter1.PesquisaNomeSabor(txtPesquisa.Text);
+
+            //Limpa os campos
+            Limpar();
+        }
+
+        //Ao clicar duas vezes em um sabor no DataGridView
+        private void dtgSabores_DoubleClick(object sender, EventArgs e)
+        {
+            //Preenche os campos com os dados do sabor selecionado
+            txtCodigo.Text = dtgSabores.Rows[dtgSabores.CurrentRow.Index].Cells["CODIGO"].Value.ToString();
+            txtNome.Text = dtgSabores.Rows[dtgSabores.CurrentRow.Index].Cells["NOME"].Value.ToString();
+            txtIngrediente.Text = dtgSabores.Rows[dtgSabores.CurrentRow.Index].Cells["INGREDIENTES"].Value.ToString();
+        }
+
+        //Quando o formulário carregar, listar os sabores
+        private void frmPizzas_Load(object sender, EventArgs e)
+        {
+            ListarSabores();
         }
     }
 }
