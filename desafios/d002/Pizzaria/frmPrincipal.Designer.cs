3753fd1b6abcda5fde76450b7be37d21b2ec9b03
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-12 23:22:40 -0300

    iniciando o layout da aba de pesquisas

--- desafios/d002/Pizzaria/frmPrincipal.Designer.cs
+++ desafios/d002/Pizzaria/frmPrincipal.Designer.cs
@@ -33,9 +33,12 @@
             this.btnClientes = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.btnPizzas = new System.Windows.Forms.ToolStripButton();
-            this.btnpedidos = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnpedidos = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -80,6 +83,11 @@
             this.btnPizzas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnPizzas.Click += new System.EventHandler(this.btnPizzas_Click);
             // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 59);
+            // 
             // btnpedidos
             // 
             this.btnpedidos.Image = global::Pizzaria.Properties.Resources.pedido;
@@ -91,16 +99,32 @@
             this.btnpedidos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnpedidos.Click += new System.EventHandler(this.btnpedidos_Click);
             // 
-            // toolStripSeparator2
+            // groupBox1
             // 
-            this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 59);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 137);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pesquisa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Situação do pedido:";
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -109,6 +133,8 @@
             this.Text = "Sistema de Controle de Pizzaria";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +148,8 @@
         private System.Windows.Forms.ToolStripButton btnPizzas;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
