e8b0d9b7110d81864757a878f7cb9d4e89d98f73
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-14 13:35:29 -0300

    lógica para fazer as pesquisas

--- desafios/d002/Pizzaria/frmPrincipal.cs
+++ desafios/d002/Pizzaria/frmPrincipal.cs
@@ -32,6 +32,58 @@ namespace Pizzaria
         }
 
         private void btnpedidos_Click(object sender, EventArgs e) => new frmPedido().ShowDialog();
-        
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            dtgPedido.DataSource = pedidoTableAdapter1.RetornarPedidos();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            if (chkEntregue.Checked)
+            {
+                dtgPedido.DataSource = pedidoTableAdapter1.RetornarEntregue();
+            }
+            else if (chkEspera.Checked)
+            {
+                dtgPedido.DataSource = pedidoTableAdapter1.RetornarEspera();
+            }
+            else if (String.IsNullOrEmpty(txtNomeCliente.Text))
+            {
+                dtgPedido.DataSource = pedidoTableAdapter1.RetornarPedidos();
+            }
+            else
+            {
+                dtgPedido.DataSource = pedidoTableAdapter1.RetornarCliente(txtNomeCliente.Text);
+            }
+        }
+
+        private void chkEntregue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkEntregue.Checked)
+            {
+                chkEspera.Enabled= false;
+                txtNomeCliente.Enabled = false;
+            }
+            else
+            {
+                chkEspera.Enabled = true;
+                txtNomeCliente.Enabled = true;
+            }
+        }
+
+        private void chkEspera_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkEspera.Checked)
+            {
+                chkEntregue.Enabled = false;
+                txtNomeCliente.Enabled = false;
+            }
+            else
+            {
+                chkEntregue.Enabled = true;
+                txtNomeCliente.Enabled = true;
+            }
+        }
     }
 }
