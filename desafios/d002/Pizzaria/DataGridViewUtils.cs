38008385dedb91aaf355b5e550ef171e8aee85c9
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-16 16:12:06 -0300

    criei a classe responsável por tratar os dtg

---
+++ desafios/d002/Pizzaria/DataGridViewUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizzaria
+{
+    internal static class DataGridViewUtils
+    {
+        public static void AjustaBarraVertical(params DataGridView[] dtgs)
+        {
+            foreach (var dtg in dtgs)
+            {
+                if (dtg == null) continue;
+                if (dtg.Rows.Count == 0) continue;
+
+                dtg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+                dtg.Columns[dtg.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill; 
+            }
+        }
+    }
+}
