c6e007023329e0b5de657df14300e2ff6a23139e
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-21 17:34:48 -0300

    desenvolvendo a classe responsável pela conexão com o banco de dados

---
+++ desafios/d003/Academia/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academia
+{
+    internal class Conexao
+    {
+        private static string conexao = "";
+
+        public static string StringConexao
+        {
+            get { return conexao; }
+        }
+    }
+}
