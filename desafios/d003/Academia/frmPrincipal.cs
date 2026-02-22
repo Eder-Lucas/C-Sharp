9fb8f1d8d7706c9cae6f1741e6afdc1d2e3d34a1
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-22 17:42:30 -0300

    finalizando o evento Load do frmPrincipal
    - Concluindo a conexão com o banco de dados

--- desafios/d003/Academia/frmPrincipal.cs
+++ desafios/d003/Academia/frmPrincipal.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.SqlClient;
+
 namespace Academia
 {
     public partial class frmPrincipal : Form
@@ -7,9 +9,25 @@ namespace Academia
             InitializeComponent();
         }
 
+        // Evento Load do formulário principal
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
+            // Estabelece a conexão com o banco de dados através da classe Conexao e seu método StringConexao
+            using SqlConnection novaConexao = new(Conexao.StringConexao);
 
+            try
+            {
+                novaConexao.Open(); // Abre a conexão com o banco
+                MessageBox.Show("Conexão com o banco de dados realizada!"); // Mensagem de sucesso caso a conexão seja estabelecida
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro ao tentar se conectar ao banco de dados!"); // Mensagem de erro caso a conexão falhe
+            }
+            finally
+            {
+                MessageBox.Show("Bem Vindo ao sistema de academia!");
+            }
         }
     }
 }
