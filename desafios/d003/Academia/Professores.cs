29bea772cac3908f543cdcb20b8fc99f2b99f5d3
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-23 12:41:06 -0300

    iniciando o desenvolvimento da classe professores
    - classe responsável por salvar, excluir ou modificar dados dos professores no banco de dados, sem utilizar os mecanismos de assistência do VS

---
+++ desafios/d003/Academia/Professores.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academia
+{
+    internal class Professores
+    {
+        public void Salvar(
+			string nome,
+			string endereco,
+			string numero,
+			string bairro,
+			string cidade,
+			string cep,
+			string cpf, 
+			decimal salario, 
+			string telefone, 
+			string observacao)
+        {
+			try
+			{
+				SqlCommand comandoSql = new SqlCommand();
+				StringBuilder sql = new StringBuilder();
+
+                using SqlConnection conexao = new(Conexao.StringConexao);
+				conexao.Open();
+
+				sql.Append("INSERT INTO Professor (NOME_PROFESSOR, ENDERECO_PROFESSOR, NUMERO_PROFESSOR, BAIRRO_PROFESSOR,");
+				sql.Append("CIDADE_PROFESSOR, CEP_PROFESSOR, CPF_PROFESSOR, SALARIO_PROFESSOR, TELEFONE_PROFESSOR, OBSERVACAO_PROFESSOR)");
+				sql.Append("VALUES (@nome, @endereco, @bairro, @cidade, @cep, @cpf, @salario, @telefone, @observacao)");
+
+				comandoSql.Parameters.Add(new SqlParameter("@nome", nome));
+                comandoSql.Parameters.Add(new SqlParameter("@endereco", endereco));
+                comandoSql.Parameters.Add(new SqlParameter("@numero", numero));
+                comandoSql.Parameters.Add(new SqlParameter("@bairro", bairro));
+                comandoSql.Parameters.Add(new SqlParameter("@cidade", cidade));
+				comandoSql.Parameters.Add(new SqlParameter("@cep", cep));
+				comandoSql.Parameters.Add(new SqlParameter("@cpf", cpf));
+				comandoSql.Parameters.Add(new SqlParameter("@salario", salario));
+				comandoSql.Parameters.Add(new SqlParameter("@telefone", telefone));
+				comandoSql.Parameters.Add(new SqlParameter("@observacao", observacao));
+
+				comandoSql.CommandText = sql.ToString();
+				comandoSql.Connection = conexao;
+				comandoSql.ExecuteNonQuery();
+            }
+			catch (Exception)
+			{
+				throw new Exception("Ocorreu um erro ao salvar. Caso o erro persista, entre em contato com o Administrador do sistema");
+			}
+        }
+    }
+}
