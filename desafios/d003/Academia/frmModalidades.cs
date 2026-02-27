e19d2b26da63cbd01958c7f7184ad6d58cc1f286
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-27 16:36:53 -0300

    criei o método que CarregaProfessores no frmModalidades
    - esse método carrega o nome dos  professores dentro do ComboBox referente aos professores
    - Além disso, chamei o método no evento Load do formulário

--- desafios/d003/Academia/frmModalidades.cs
+++ desafios/d003/Academia/frmModalidades.cs
@@ -14,5 +14,28 @@ namespace Academia
         {
             InitializeComponent();
         }
+
+        public void CarregarProfessores()
+        {
+            try
+            {
+                Professores novoProfessor = new();
+
+                cboProfessor.DataSource = novoProfessor.Listar();
+                cboProfessor.DisplayMember = "NOME_PROFESSOR";
+                cboProfessor.ValueMember = "ID_PROFESSOR";
+
+                cboProfessor.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar os professores: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void frmModalidades_Load(object sender, EventArgs e)
+        {
+            CarregarProfessores();
+        }
     }
 }
