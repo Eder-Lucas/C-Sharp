9ea7b32b46d54dcb5a7e34095fafbdd25aa78eca
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-02-27 16:39:40 -0300

    criei a classe que gerencia operações no banco relacionadas as modalidades
    - criei também os métodos Salvar e Alterar

---
+++ desafios/d003/Academia/Modalidades.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Academia
+{
+    internal class Modalidades
+    {
+        public void Salvar(string nome, decimal mensalidade, int idProfessor)
+        {
+			try
+			{
+				using SqlConnection conexao = new(Conexao.StringConexao);
+				conexao.Open();
+
+				string sql = """
+					INSERT INTO Modalidades (NOME_MODALIDADE, MENSALIDADE, ID_PROFESSOR)
+					VALUES (@nome, @mensalidade, @idProfessor)
+				""";
+
+				using SqlCommand cmd = new(sql, conexao);
+
+				cmd.Parameters.Add("@nome", SqlDbType.NVarChar, 50).Value = nome;
+
+				var valor = cmd.Parameters.Add("@mensalidade", SqlDbType.Decimal);
+				valor.Precision = 6;
+				valor.Scale = 2;
+				valor.Value = mensalidade;
+				
+				cmd.Parameters.Add("@idProfessor", SqlDbType.Int).Value = idProfessor;
+
+				cmd.ExecuteNonQuery();
+            }
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message, ex);
+			}
+        }
+
+		public void Alterar(int idModalidade, string nome, decimal mensalidade, int idProfessor)
+		{
+			try
+			{
+				using SqlConnection conexao = new(Conexao.StringConexao);
+				conexao.Open();
+
+				string sql = """
+					UPADATE Modalidade
+					SET ID_MODALIDADE = @idModalidade,
+						NOME_MODALIDADE = @nome,
+						MENSALIDADE = @mensalidade,
+						ID_PROFESSOR = @idProfessor
+					WHERE ID_MODALIDADE = @idModalidade
+				""";
+
+				using SqlCommand cmd = new(sql, conexao);
+
+				cmd.Parameters.Add("@idModalidade", SqlDbType.Int).Value = idModalidade;
+				cmd.Parameters.Add("@nome", SqlDbType.NVarChar, 50).Value = nome;
+
+                var valor = cmd.Parameters.Add("@mensalidade", SqlDbType.Decimal);
+                valor.Precision = 6;
+                valor.Scale = 2;
+                valor.Value = mensalidade;
+
+				cmd.Parameters.Add("@idProfessor", SqlDbType.Int).Value = idProfessor;
+
+				cmd.ExecuteNonQuery();
+            }
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message, ex);
+            }
+		}
+    }
+}
