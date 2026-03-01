7dcf9ee8a72c69f548c489979d72f9e44cd90576
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-01 17:07:13 -0300

    criei a classe de utilidades do DataGridView

---
+++ desafios/d003/Academia/DataGridViewUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Academia
+{
+    internal class DataGridViewUtils
+    {
+        private static readonly Dictionary<DataGridView, HashSet<string>> mapaColunas = new();
+
+        private static void Dtg_MouseCellEnter(object? sender, DataGridViewCellEventArgs e)
+        {
+            if (sender is not DataGridView dtg) return;
+
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+
+            if (mapaColunas.TryGetValue(dtg, out var col))
+            {
+                string nomeColuna = dtg.Columns[e.ColumnIndex].Name;
+
+                if (col.Contains(nomeColuna))
+                    dtg.Cursor = Cursors.Hand;
+            }
+        }
+
+        private static void Dtg_MouseCellLeave(object? sender, DataGridViewCellEventArgs e)
+        {
+            if (sender is not DataGridView dtg) return;
+
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+
+            dtg.Cursor = Cursors.Default;
+        }
+
+        public static void HandButton(DataGridView dtg, params string[] col)
+        {
+            var colunas = new HashSet<string>(col);
+
+            mapaColunas[dtg] = colunas;
+
+            dtg.CellMouseLeave -= Dtg_MouseCellLeave;
+            dtg.CellMouseEnter -= Dtg_MouseCellEnter;
+
+            dtg.CellMouseEnter += Dtg_MouseCellEnter;
+            dtg.CellMouseLeave += Dtg_MouseCellLeave;
+        }
+    }
+}
