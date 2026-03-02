3eec14052e0a6fe3ea0d2643ce648bb64595a467
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-01 21:31:15 -0300

    criei a classe responsável por formatar campos

---
+++ desafios/d003/Academia/Formatador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academia
+{
+    internal class Formatador
+    {
+        public static string Cpf(string txt)
+        {
+            string numeros = new string(txt.Where(char.IsDigit).ToArray());
+
+            string modelo = "###.###.###-##";
+
+            int n = 0;
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < modelo.Length; i++)
+            {
+                if (n >= numeros.Length) break;
+
+                if (modelo[i] == '#')
+                {
+                    sb.Append(numeros[n]);
+                    n++;
+                }
+                else
+                {
+                    sb.Append(modelo[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+}
