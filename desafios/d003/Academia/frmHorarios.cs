7cf86311b0a5a4d2ae9cbaed7e53ce867bdf5c44
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-07 22:30:28 -0300

    criando o titulo do frmHorarios dinamicamente

---
+++ desafios/d003/Academia/frmHorarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Academia
+{
+    public partial class frmHorarios : Form
+    {
+        private int codigo;
+        private string modalidade, turma;
+
+        public frmHorarios(int codigo, string modalidade, string turma)
+        {
+            InitializeComponent();
+
+            this.codigo = codigo;
+            this.modalidade = modalidade;
+            this.turma = turma;
+        }
+
+        private void frmHorarios_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Text = $"{modalidade} - turma {turma}";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
