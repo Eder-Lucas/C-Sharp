e53bb79c80765cf2ccd7f0557e4f3f0ef0c92365
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-08 17:56:26 -0300

    criando métodos e eventos
    - criei os eventos de listar horários e limpar campos
    - iniciei o desenvolvimento da lógica dos botões de salvar e adicionar horários
    - ajustei o método construtor
    - criei uma variável que controla o id horario

--- desafios/d003/Academia/frmHorarios.cs
+++ desafios/d003/Academia/frmHorarios.cs
@@ -10,14 +10,17 @@ namespace Academia
 {
     public partial class frmHorarios : Form
     {
-        private int codigo;
+        private int idTurma;
         private string modalidade, turma;
+        private int idHorario = 0;
 
-        public frmHorarios(int codigo, string modalidade, string turma)
+        private readonly Horarios novoHorario = new();
+
+        public frmHorarios(int idTurma, string modalidade, string turma)
         {
             InitializeComponent();
 
-            this.codigo = codigo;
+            this.idTurma = idTurma;
             this.modalidade = modalidade;
             this.turma = turma;
         }
@@ -27,10 +30,83 @@ namespace Academia
             try
             {
                 this.Text = $"{modalidade} - turma {turma}";
+
+                cboSemana.SelectedIndex = 0;
+
+                ListarHorarios();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ListarHorarios()
+        {
+            try
+            {
+                dtgHorarios.DataSource = novoHorario.Listar();
+
+                dtgHorarios.Columns["INICIO"]?.DefaultCellStyle.Format = @"hh\:mm";
+                dtgHorarios.Columns["FIM"]?.DefaultCellStyle.Format = @"hh\:mm";
             }
             catch (Exception)
             {
+                throw;
+            }
+        }
 
+        private void Limpar(Control parent)
+        {
+            foreach(Control c in parent.Controls)
+            {
+                if (c is TextBox txt)
+                    txt.Clear();
+                else if (c is ComboBox cbo)
+                    cbo.SelectedIndex = -1;
+                else if (c is DateTimePicker dtp)
+                    dtp.Value = DateTime.Now;
+
+                idHorario = 0;
+            }
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            Limpar(this);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TimeSpan inicio = dtpInicio.Value.TimeOfDay;
+                TimeSpan fim = dtpFim.Value.TimeOfDay;
+
+                if (idHorario == 0)
+                {
+                    novoHorario.Salvar(idTurma, cboSemana.SelectedIndex, inicio, fim);
+                    MessageBox.Show(
+                        "Modalidade salva com sucesso!",
+                        "Sucesso",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+                else
+                {
+                    novoHorario.Alterar(idHorario, idTurma, cboSemana.SelectedIndex, inicio, fim);
+                    MessageBox.Show(
+                        "Modalidade alterada com sucesso!",
+                        "Sucesso",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+
+                ListarHorarios();
+                Limpar(this);
+            }
+            catch (Exception)
+            {
                 throw;
             }
         }
