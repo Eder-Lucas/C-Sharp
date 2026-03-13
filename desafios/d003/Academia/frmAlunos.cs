dc9e972a35cd397bee1bcd6b9400a279a07cd402
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-12 21:04:38 -0300

    linkando o frmAlunos ao frmControleAlunos

---
+++ desafios/d003/Academia/frmAlunos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Academia
+{
+    public partial class frmAlunos : Form
+    {
+        public frmAlunos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e) => new frmControleAlunos().ShowDialog();
+    }
+}
