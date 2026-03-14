4f739a3477ff0244a54e8d1270078e1411b567e9
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-13 22:30:55 -0300

    criei um método e um evento no frmControleAluno
    - criei o método limpar
    - criei o evento do botão salvar, sendo possível salvar alunos agora
    - também criei algumas variáveis que serão importantes para lógicas futuras nesse form

--- desafios/d003/Academia/frmControleAlunos.cs
+++ desafios/d003/Academia/frmControleAlunos.cs
@@ -14,5 +14,59 @@ namespace Academia
         {
             InitializeComponent();
         }
+
+        private int idAluno = 0;
+        private readonly Alunos novoAluno = new();
+
+        private void Limpar(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is TextBox txt)
+                    txt.Clear();
+                else if (c is DateTimePicker dtp)
+                    dtp.Value = DateTime.Today;
+
+                if (c.HasChildren)
+                    Limpar(c);
+            }
+
+            idAluno = 0;
+            cboSexo.SelectedIndex = 0;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sexo = cboSexo.SelectedIndex == 0 ? "M" : "F";
+                bool novo = idAluno == 0;
+
+                novoAluno.Salvar(idAluno, txtNome.Text, txtEndereco.Text, txtBairro.Text, txtNumero.Text, txtCidade.Text, mtbCep.Text, mtbCpf.Text, mtbTel.Text, sexo, txtObs.Text);
+                
+                if (novo)
+                {
+                    MessageBox.Show(
+                    "Aluno salvo com sucesso!",
+                    "Sucesso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show(
+                    "Aluno alterado com sucesso!",
+                    "Sucesso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                }
+
+                Limpar(tabPageCadastro);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro ao salvar informações", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
