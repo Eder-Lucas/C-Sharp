39e3baded23e86ad98151d80f4f9af7b30dab2ea
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-14 13:06:37 -0300

    corrigindo e criando novos métodos no frmControleAlunos
    - corrigi o método limpar, agora ele percorre os maskedTextBox e trata corretamente do txtCod e cboSexo ao limpar
    - criei um método para validar os campos antes de salvar
    - criei um método que pega o último aluno a ser cadastrado e mostra suas informações no tabPageMatricula
    - além disso, criei uma lógica para quando salvar perguntar ao usuário se o mesmo já quer matricular esse aluno numa turma

--- desafios/d003/Academia/frmControleAlunos.cs
+++ desafios/d003/Academia/frmControleAlunos.cs
@@ -24,15 +24,16 @@ namespace Academia
             {
                 if (c is TextBox txt)
                     txt.Clear();
-                else if (c is DateTimePicker dtp)
-                    dtp.Value = DateTime.Today;
+                else if (c is MaskedTextBox mtb)
+                    mtb.Clear();
 
                 if (c.HasChildren)
                     Limpar(c);
             }
 
             idAluno = 0;
-            cboSexo.SelectedIndex = 0;
+            txtCod.Text = "0";
+            cboSexo.SelectedIndex = -1;
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
@@ -41,6 +42,8 @@ namespace Academia
             {
                 var sexo = cboSexo.SelectedIndex == 0 ? "M" : "F";
                 bool novo = idAluno == 0;
+
+                ValidaCampos(tabPageCadastro);
 
                 novoAluno.Salvar(idAluno, txtNome.Text, txtEndereco.Text, txtBairro.Text, txtNumero.Text, txtCidade.Text, mtbCep.Text, mtbCpf.Text, mtbTel.Text, sexo, txtObs.Text);
                 
@@ -51,6 +54,16 @@ namespace Academia
                     "Sucesso",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Information);
+
+                    bool matricula = MessageBox.Show(
+                        "Deseja realizar a matrícula do aluno agora?",
+                        "Realizar matrícula?",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question) == DialogResult.Yes;
+
+                    if (matricula) tcAluno.SelectedTab = tabPageMatricula;
+
+                    ListaUltimoAluno();
                 }
                 else
                 {
@@ -66,6 +79,49 @@ namespace Academia
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Erro ao salvar informações", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ValidaCampos(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is TextBox txt)
+                {
+                    if (string.IsNullOrWhiteSpace(txt.Text) && txt.Name != "txtObs")
+                        throw new Exception("Preencha todos os campos corretamente!");   
+                }
+
+                if (c is MaskedTextBox mtb)
+                {
+                    if (!mtb.MaskFull)
+                        throw new Exception("Preencha todos os campos corretamente!");
+                }
+
+                if (c is ComboBox cbo)
+                {
+                    if (cbo.SelectedIndex == -1)
+                        throw new Exception("Preencha todos os campos corretamente!");
+                }      
+                
+                if (c.HasChildren)
+                    ValidaCampos(c);
+            }
+        }
+
+        private void ListaUltimoAluno()
+        {
+            try
+            {
+                DataTable dadosTabela = new();
+                dadosTabela = novoAluno.Listar();
+
+                txtCodAluno.Text = dadosTabela.Rows[0]["ID_ALUNO"].ToString();
+                txtNomeAluno.Text = dadosTabela.Rows[0]["NOME_ALUNO"].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro ao listar aluno", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
