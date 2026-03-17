3e4e2f9e6219238b1d18d716c317d06696791453
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-03-17 17:38:54 -0300

    criei a classe Matriculas para gerenciar as operações das matrículas no banco

---
+++ desafios/d003/Academia/Matriculas.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Academia
+{
+    internal class Matriculas
+    {
+        public void Salvar(int idMatricula, int idAluno, int idTurma, DateTime venc, bool situacao)
+        {
+            try
+            {
+                if (idMatricula == 0)
+                    Inserir(idAluno, idTurma, venc, situacao);
+                else
+                    Atualizar(idMatricula, idAluno, idTurma, venc, situacao);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public void Inserir(int idAluno, int idTurma, DateTime venc, bool situacao)
+        {
+            try
+            {
+                using SqlConnection conexao = new SqlConnection(Conexao.StringConexao);
+                conexao.Open();
+
+                string sql = """
+                    INSERT INTO Matricula (ID_ALUNO, ID_TURMA, VENCIMENTO, SITUACAO)
+                    VALUES (@idAluno, @idTurma, @venc, @situacao)
+                """;
+
+                using SqlCommand cmd = new SqlCommand(sql, conexao);
+
+                cmd.Parameters.Add("@idAluno", System.Data.SqlDbType.Int).Value = idAluno;
+                cmd.Parameters.Add("@idTurma", System.Data.SqlDbType.Int).Value = idTurma;
+                cmd.Parameters.Add("@venc", System.Data.SqlDbType.Date).Value = venc;
+                cmd.Parameters.Add("@situacao", System.Data.SqlDbType.Bit).Value = situacao;
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public void Atualizar(int idMatricula, int idAluno, int idTurma, DateTime venc, bool situacao)
+        {
+            try
+            {
+                using SqlConnection conexao = new SqlConnection(Conexao.StringConexao);
+                conexao.Open();
+
+                string sql = """
+                    UPDATE Matricula
+                    SET ID_ALUNO = @idAluno,
+                        ID_TURMA = @idTurma,
+                        VENCIMENTO = @venc,
+                        SITUACAO = @situacao
+                    WHERE ID_MATRICULA = @idMatricula
+                """;
+
+                using SqlCommand cmd = new SqlCommand(sql, conexao);
+
+                cmd.Parameters.Add("@idAluno", System.Data.SqlDbType.Int).Value = idAluno;
+                cmd.Parameters.Add("@idTurma", System.Data.SqlDbType.Int).Value = idTurma;
+                cmd.Parameters.Add("@venc", System.Data.SqlDbType.Date).Value = venc;
+                cmd.Parameters.Add("@situacao", System.Data.SqlDbType.Bit).Value = situacao;
+                cmd.Parameters.Add("@idMatricula", System.Data.SqlDbType.Int).Value = idMatricula;
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Excluir(int idMatricula)
+        {
+            try
+            {
+                using SqlConnection conexao = new(Conexao.StringConexao);
+                conexao.Open();
+
+                string sql = """
+                    DELETE FROM Matricula
+                    WHERE ID_MATRICULA = @idMatricula
+                """;
+
+                using SqlCommand cmd = new(sql, conexao);
+
+                cmd.Parameters.Add("@idMatricula", System.Data.SqlDbType.Int).Value = idMatricula;
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DataTable ListarTurmas()
+        {
+            try
+            {
+                using SqlConnection conexao = new(Conexao.StringConexao);
+                conexao.Open();
+
+                string sql = """
+                    SELECT * FROM Turma
+                    ORDER BY ID_TURMA DESC
+                """;
+
+                using SqlCommand cmd = new(sql, conexao);
+
+                DataTable tabela = new();
+
+                using SqlDataReader leitor = cmd.ExecuteReader();
+                tabela.Load(leitor);
+
+                return tabela;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DataTable RetornarMatriculas(int idAluno)
+        {
+            try
+            {
+                using SqlConnection conexao = new(Conexao.StringConexao);
+                conexao.Open();
+
+                string sql = """
+                    SELECT * FROM Matricula
+                    WHERE ID_ALUNO = @idAluno
+                    ORDER BY ID_MATRICULA DESC
+                """;
+
+                using SqlCommand cmd = new(sql, conexao);
+                cmd.Parameters.Add("@idAluno", System.Data.SqlDbType.Int).Value = idAluno;
+
+                DataTable tabela = new();
+
+                using SqlDataReader leitor = cmd.ExecuteReader();
+                tabela.Load(leitor);
+
+                return tabela;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public DataTable RetornarTurmasMatriculadas(int idAluno)
+        {
+            try
+            {
+                using SqlConnection conexao = new(Conexao.StringConexao);
+                conexao.Open();
+
+                string sql = """
+                    SELECT m.*, t.NUMERO_TURMA, md.NOME_MODALIDADE, md.MENSALIDADE
+                    FROM Matricula m
+                    INNER JOIN Turma t ON m.ID_TURMA = t.ID_TURMA
+                    INNER JOIN Aluno a ON m.ID_ALUNO = a.ID_ALUNO
+                    INNER JOIN Modalidade md ON t.ID_MODALIDADE = md.ID_MODALIDADE
+                    WHERE m.ID_ALUNO = @idAluno
+                    ORDER BY m.ID_MATRICULA DESC
+                """;
+
+                using SqlCommand cmd = new(sql, conexao);
+                cmd.Parameters.Add("@idAluno", System.Data.SqlDbType.Int).Value = idAluno;
+
+                DataTable tabela = new();
+
+                using SqlDataReader leitor = cmd.ExecuteReader();
+                tabela.Load(leitor);
+
+                return tabela;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
