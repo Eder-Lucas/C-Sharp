8f5565dd519145b1ba1f4dffffed1fb7c82a785f
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-04-06 18:02:15 -0300

    nomeando e ajustando alguns componentes do frmAberturaCaixa

--- desafios/d003/Academia/frmAberturaCaixa.Designer.cs
+++ desafios/d003/Academia/frmAberturaCaixa.Designer.cs
@@ -29,18 +29,15 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            comboBox1 = new ComboBox();
+            cboValorInicial = new ComboBox();
             label2 = new Label();
             pictureBox3 = new PictureBox();
             label1 = new Label();
             tcCaixa = new TabControl();
             tabPageDadosCaixa = new TabPage();
             label5 = new Label();
-            label6 = new Label();
-            label4 = new Label();
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
+            txtCaixa = new TextBox();
+            txtDinheiro = new TextBox();
             label3 = new Label();
             btnAbrirCaixa = new Button();
             button1 = new Button();
@@ -53,7 +50,7 @@
             // panel1
             // 
             panel1.BackColor = Color.White;
-            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(cboValorInicial);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(pictureBox3);
             panel1.Controls.Add(label1);
@@ -62,16 +59,17 @@
             panel1.Size = new Size(385, 109);
             panel1.TabIndex = 0;
             // 
-            // comboBox1
+            // cboValorInicial
             // 
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Zerado", "Com os valores que eu digitar", "Com o saldo do caixa anterior" });
-            comboBox1.Location = new Point(79, 74);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(299, 25);
-            comboBox1.TabIndex = 1;
+            cboValorInicial.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboValorInicial.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cboValorInicial.FormattingEnabled = true;
+            cboValorInicial.Items.AddRange(new object[] { "Zerado", "Com os valores que eu digitar", "Com o saldo do caixa anterior" });
+            cboValorInicial.Location = new Point(79, 74);
+            cboValorInicial.Name = "cboValorInicial";
+            cboValorInicial.Size = new Size(299, 25);
+            cboValorInicial.TabIndex = 1;
+            cboValorInicial.SelectedIndexChanged += cboValorInicial_SelectedIndexChanged;
             // 
             // label2
             // 
@@ -109,22 +107,19 @@
             tcCaixa.Location = new Point(12, 127);
             tcCaixa.Name = "tcCaixa";
             tcCaixa.SelectedIndex = 0;
-            tcCaixa.Size = new Size(389, 148);
+            tcCaixa.Size = new Size(389, 114);
             tcCaixa.TabIndex = 1;
             // 
             // tabPageDadosCaixa
             // 
             tabPageDadosCaixa.Controls.Add(label5);
-            tabPageDadosCaixa.Controls.Add(label6);
-            tabPageDadosCaixa.Controls.Add(label4);
-            tabPageDadosCaixa.Controls.Add(textBox3);
-            tabPageDadosCaixa.Controls.Add(textBox2);
-            tabPageDadosCaixa.Controls.Add(textBox1);
+            tabPageDadosCaixa.Controls.Add(txtCaixa);
+            tabPageDadosCaixa.Controls.Add(txtDinheiro);
             tabPageDadosCaixa.Controls.Add(label3);
             tabPageDadosCaixa.Location = new Point(4, 29);
             tabPageDadosCaixa.Name = "tabPageDadosCaixa";
             tabPageDadosCaixa.Padding = new Padding(3);
-            tabPageDadosCaixa.Size = new Size(381, 115);
+            tabPageDadosCaixa.Size = new Size(381, 81);
             tabPageDadosCaixa.TabIndex = 0;
             tabPageDadosCaixa.Text = "Dados do caixa";
             tabPageDadosCaixa.UseVisualStyleBackColor = true;
@@ -133,61 +128,31 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(15, 82);
+            label5.Location = new Point(15, 48);
             label5.Name = "label5";
             label5.Size = new Size(180, 19);
             label5.TabIndex = 13;
             label5.Text = "Valor inicial em CAIXA:";
             // 
-            // label6
+            // txtCaixa
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(8, 48);
-            label6.Name = "label6";
-            label6.Size = new Size(187, 19);
-            label6.TabIndex = 12;
-            label6.Text = "Valor inicial em Cheque:";
+            txtCaixa.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtCaixa.Location = new Point(201, 45);
+            txtCaixa.Name = "txtCaixa";
+            txtCaixa.Size = new Size(164, 25);
+            txtCaixa.TabIndex = 2;
+            txtCaixa.Text = "0,00";
+            txtCaixa.TextAlign = HorizontalAlignment.Right;
             // 
-            // label4
+            // txtDinheiro
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(3, 48);
-            label4.Name = "label4";
-            label4.Size = new Size(192, 19);
-            label4.TabIndex = 12;
-            label4.Text = "Valor inicial em Dinheiro:";
-            // 
-            // textBox3
-            // 
-            textBox3.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox3.Location = new Point(201, 79);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(164, 25);
-            textBox3.TabIndex = 2;
-            textBox3.Text = "0,00";
-            textBox3.TextAlign = HorizontalAlignment.Right;
-            // 
-            // textBox2
-            // 
-            textBox2.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(201, 45);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(164, 25);
-            textBox2.TabIndex = 2;
-            textBox2.Text = "0,00";
-            textBox2.TextAlign = HorizontalAlignment.Right;
-            // 
-            // textBox1
-            // 
-            textBox1.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(201, 11);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(164, 25);
-            textBox1.TabIndex = 2;
-            textBox1.Text = "0,00";
-            textBox1.TextAlign = HorizontalAlignment.Right;
+            txtDinheiro.Font = new Font("Arial", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtDinheiro.Location = new Point(201, 11);
+            txtDinheiro.Name = "txtDinheiro";
+            txtDinheiro.Size = new Size(164, 25);
+            txtDinheiro.TabIndex = 2;
+            txtDinheiro.Text = "0,00";
+            txtDinheiro.TextAlign = HorizontalAlignment.Right;
             // 
             // label3
             // 
@@ -204,7 +169,7 @@
             btnAbrirCaixa.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnAbrirCaixa.Image = Properties.Resources.cadeado_aberto;
             btnAbrirCaixa.ImageAlign = ContentAlignment.MiddleLeft;
-            btnAbrirCaixa.Location = new Point(177, 281);
+            btnAbrirCaixa.Location = new Point(178, 248);
             btnAbrirCaixa.Name = "btnAbrirCaixa";
             btnAbrirCaixa.Size = new Size(132, 50);
             btnAbrirCaixa.TabIndex = 17;
@@ -216,7 +181,7 @@
             // 
             button1.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(315, 280);
+            button1.Location = new Point(316, 247);
             button1.Name = "button1";
             button1.Size = new Size(82, 51);
             button1.TabIndex = 18;
@@ -227,7 +192,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(410, 349);
+            ClientSize = new Size(410, 311);
             Controls.Add(button1);
             Controls.Add(btnAbrirCaixa);
             Controls.Add(tcCaixa);
@@ -252,7 +217,7 @@
         private PictureBox pictureBox3;
         private Label label2;
         private Label label1;
-        private ComboBox comboBox1;
+        private ComboBox cboValorInicial;
         private TabControl tcCaixa;
         private TabPage tabPageDadosCaixa;
         private Label label3;
@@ -261,7 +226,8 @@
         private Label label4;
         private TextBox textBox3;
         private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox txtCaixa;
+        private TextBox txtDinheiro;
         private Button btnAbrirCaixa;
         private Button button1;
     }
