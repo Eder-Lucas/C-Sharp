5eb1abc08693099971f4f1cb266ac38176d5ec94
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-04-07 23:54:35 -0300

    desenvolvendo a lógica da abertura de caixa

--- desafios/d003/Academia/frmAberturaCaixa.cs
+++ desafios/d003/Academia/frmAberturaCaixa.cs
@@ -26,16 +26,69 @@ namespace Academia
         private void switchZerado_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
-            {
                 switchSaldoAnterior.Checked = false;
-            }
         }
 
         private void switchSaldoAnterior_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
-            {
                 switchZerado.Checked = false;
+        }
+
+        private void btnCancelarAbertura_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAbrirCaixa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!switchZerado.Checked && txtTotal.Text == "0,00")
+                {
+                    MessageBox.Show("pode nao ser zero", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    Caixa novoCaixa = new();
+
+                    novoCaixa.Salvar(DateTime.Today, DateTime.Now, Convert.ToDecimal(txtTotal.Text), true);
+                    this.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void switchZerado_OnCheckedChanged(object sender, EventArgs e)
+        {
+            if (switchZerado.Checked)
+            {
+                txtTotal.Enabled = false;
+                txtSaldoAnterior.Enabled = false;
+                txtValorAbertura.Enabled = false;
+            }
+            else
+            {
+                txtSaldoAnterior.Enabled = true;
+                txtTotal.Enabled = true;
+                txtValorAbertura.Enabled = true;
+
+            }
+        }
+
+        private void switchSaldoAnterior_OnCheckedChanged(object sender, EventArgs e)
+        {
+            if (switchSaldoAnterior.Checked)
+            {
+                txtSaldoAnterior.Enabled = false;
+            }
+            else
+            {
+                txtSaldoAnterior.Enabled = true;
             }
         }
     }
