9ae6f5c159717af755613dc6c0f104aa699ea55e
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-04-07 23:55:20 -0300

    iniciando a lógica do frmSuprimento

--- desafios/d003/Academia/frmSuprimento.cs
+++ desafios/d003/Academia/frmSuprimento.cs
@@ -10,9 +10,30 @@ namespace Academia
 {
     public partial class frmSuprimento : Form
     {
-        public frmSuprimento()
+        int idMensalidade;
+        string valor;
+        frmControleAlunos frmControleAlunos;
+
+        public frmSuprimento(int idMensalidade, decimal valor, frmControleAlunos frmControleAlunos)
         {
             InitializeComponent();
+
+            this.idMensalidade = idMensalidade;
+            this.valor = valor.ToString("c");
+            txtDinheiro.Text = this.valor;
+            this.frmControleAlunos = frmControleAlunos;
+        }
+
+        private readonly Caixa novoCaixa = new();
+        private readonly Mensalidades novaMensalidade = new();
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            var forma = cboFormaPagamento.SelectedIndex.ToString();
+            novoCaixa.SalvarTransacao();
         }
     }
 }
