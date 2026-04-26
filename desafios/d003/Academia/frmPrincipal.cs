480d4070c4c8e640f70e32c7d8d7034c0181ce00
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-04-25 22:42:27 -0300

    criei o método VerificaSituacaoCaixa
    - Esse método vai atualizar o label dependendo da situação do caixa

--- desafios/d003/Academia/frmPrincipal.cs
+++ desafios/d003/Academia/frmPrincipal.cs
@@ -1,4 +1,5 @@
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace Academia
 {
@@ -9,43 +10,63 @@ namespace Academia
             InitializeComponent();
         }
 
-        // Evento Load do formulário principal
+        private readonly Caixa novoCaixa = new();
+
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
             // Estabelece a conexão com o banco de dados através da classe Conexao e seu método StringConexao
             using SqlConnection novaConexao = new(Conexao.StringConexao);
 
-            // Tenta abrir a conexão com o banco de dados
             try
             {
-                novaConexao.Open(); // Abre a conexão com o banco
-                MessageBox.Show("Conexão com o banco de dados realizada!"); // Mensagem de sucesso caso a conexão seja estabelecida
+                novaConexao.Open(); 
+                MessageBox.Show("Conexão com o banco de dados realizada!"); 
 
                 MatriculaService matriculaService = new();
                 matriculaService.GerarMensalidade();
+
+                VerificaSituacaoCaixa();
             }
-            // Se falhar captura o erro e exibe uma mensagem de falha
             catch (Exception ex)
             {
-                MessageBox.Show($"Erro ao tentar se conectar ao banco de dados: {ex.Message}"); // Mensagem de erro caso a conexão falhe
+                MessageBox.Show($"Erro ao tentar se conectar ao banco de dados: {ex.Message}"); 
             }
-            // Executa sempre, independentemente do sucesso ou falha da conexão
+            // Executa sempre
             finally
             {
-                MessageBox.Show("Bem Vindo ao sistema de academia!"); // Exibe independente do resultado da conexão
+                MessageBox.Show("Bem Vindo ao sistema de academia!"); 
             }
         }
 
         private void btnProfessor_Click(object sender, EventArgs e) => new frmProfessores().ShowDialog();
-
         private void btnModalidades_Click(object sender, EventArgs e) => new frmModalidades().ShowDialog();
-
         private void btnTurmas_Click(object sender, EventArgs e) => new frmTurmas().ShowDialog();
-
         private void btnAlunos_Click(object sender, EventArgs e) => new frmAlunos().ShowDialog();
-
-        private void btnCaixa_Click(object sender, EventArgs e) => new frmCaixa().ShowDialog();
-
+        private void btnCaixa_Click(object sender, EventArgs e) => new frmCaixa(this).ShowDialog();
         private void btnConfig_Click(object sender, EventArgs e) => new frmConfig().ShowDialog();
+
+        public void VerificaSituacaoCaixa()
+        {
+            try
+            {
+                DataTable dadosCaixa = novoCaixa.Listar();
+                bool situacaoCaixa = Convert.ToBoolean(dadosCaixa.Rows[0]["SITUACAO"]);
+
+                if (situacaoCaixa)
+                {
+                    lblCaixa.Text = "ABERTO";
+                    lblCaixa.ForeColor = Color.DarkGreen;
+                }
+                else
+                {
+                    lblCaixa.Text = "FECHADO";
+                    lblCaixa.ForeColor= Color.Firebrick;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
