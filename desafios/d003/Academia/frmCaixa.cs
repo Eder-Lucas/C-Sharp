526a8082f419ed42b3d370cae39a012efe83808f
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-04-25 22:45:18 -0300

    criei a lógica de fechamento do caixa
    - agora é possível fechar uma caixa

--- desafios/d003/Academia/frmCaixa.cs
+++ desafios/d003/Academia/frmCaixa.cs
@@ -10,11 +10,42 @@ namespace Academia
 {
     public partial class frmCaixa : Form
     {
-        public frmCaixa()
+        public frmPrincipal formularioPrincipal;
+        public frmCaixa(frmPrincipal formularioPrincipal)
         {
             InitializeComponent();
+            this.formularioPrincipal = formularioPrincipal;
         }
 
+        private readonly Caixa novoCaixa = new();
+
         private void btnAbrirCaixa_Click(object sender, EventArgs e) => new frmAberturaCaixa().ShowDialog();
+
+        private void btnFecharCaixa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool fechar = MessageBox.Show(
+                    "Deseja realmente fechar o caixa?", 
+                    "Deseja fechar?",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes;
+
+                if (fechar)
+                {
+                    DataTable dadosCaixa = novoCaixa.Listar();
+                    int idCaixa = Convert.ToInt32(dadosCaixa.Rows[0]["ID_CAIXA"]);
+
+                    novoCaixa.AlterarSituacao(idCaixa, false);
+                    formularioPrincipal.VerificaSituacaoCaixa();
+
+                    MessageBox.Show("Caixa fechado com sucesso!", "Caixa fechado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
