217c712a2b97ad58055465a7f2c217a2908dc040
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-04-27 12:23:25 -0300

    iniciando a lógica do suprimento

--- desafios/d003/Academia/frmSuprimento.cs
+++ desafios/d003/Academia/frmSuprimento.cs
@@ -10,9 +10,40 @@ namespace Academia
 {
     public partial class frmSuprimento : Form
     {
-        public frmSuprimento()
+        private readonly frmCaixa formCaixa;
+
+        public frmSuprimento(frmCaixa formulario)
         {
             InitializeComponent();
+            this.formCaixa = formulario;
+        }
+
+        private readonly Caixa novoCaixa = new();
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            // Armazena os dados para salvar a transação de suprimento
+            DataTable dadosCaixa = novoCaixa.Listar();
+            int idCaixa = Convert.ToInt32(dadosCaixa.Rows[0]["ID_CAIXA"]);
+            decimal valor = Convert.ToDecimal(txtValor.Text);
+            string forma = cboFormaPagamento.Text;
+
+            // Executa o suprimento
+            novoCaixa.SalvarTransacao(idCaixa, valor, "E", forma);
+            MessageBox.Show(
+            "Suprimento realizado com sucesso!",
+            "Sucesso",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+
+            formCaixa.ListarDetalhesCaixa();
+
+            this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
