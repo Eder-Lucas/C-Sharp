6be92a56561065c2b14d7c4e80045f3204b91685
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-05-02 17:04:52 -0300

    criei a classe responsável pela navegação dos UserControls

---
+++ desafios/d003/Academia/Navegacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academia
+{
+    internal static class Navegacao
+    {
+        public static Panel? PanelPrincipal { get; set; }
+
+        private static UserControl? ucAtivo;
+
+        public static void AbrirUc(UserControl uc)
+        {
+            // Evita recarregar a mesma tela e também verifica se o painel foi definido
+            if (ucAtivo == uc || PanelPrincipal == null) return; 
+
+            PanelPrincipal.Controls.Remove(ucAtivo);    
+            uc.Dock = DockStyle.Fill;
+            PanelPrincipal.Controls.Add(uc);
+
+            ucAtivo = uc;
+        }
+    }
+}
