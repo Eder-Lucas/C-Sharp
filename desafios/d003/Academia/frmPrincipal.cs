d439589c6a2bf6df02c8856ab4f5ec2fc25a5afb
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-05-06 21:32:16 -0300

    feat: adiciona um estado de selecionado aos botões do menu
    - ao clicar em um botão do menu lateral, ele recebe um aspecto de "selecionado"
    - foi criado um mapeamento com Dictionary para controlar os ícones de seleção
    - o painel indicador acompanha o botão selecionado

--- desafios/d003/Academia/frmPrincipal.cs
+++ desafios/d003/Academia/frmPrincipal.cs
@@ -10,6 +10,9 @@ namespace Academia
             InitializeComponent();
 
             this.WindowState = FormWindowState.Maximized;
+            this.DoubleBuffered = true;
+
+            InicializaIcones();
         }
 
         private readonly Caixa novoCaixa = new();
@@ -21,6 +24,8 @@ namespace Academia
         private readonly ucAlunos alunos = new();
         private ucCaixa? caixa = null;
 
+        private Dictionary<Button, (Image normal, Image ativo)> icones;
+
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
             // Estabelece a conexão com o banco de dados através da classe Conexao e seu método StringConexao
@@ -30,8 +35,8 @@ namespace Academia
             {
                 Navegacao.PanelPrincipal = pnlConteudo;
 
-                novaConexao.Open(); 
-                MessageBox.Show("Conexão com o banco de dados realizada!"); 
+                novaConexao.Open();
+                MessageBox.Show("Conexão com o banco de dados realizada!");
 
                 MatriculaService matriculaService = new();
                 matriculaService.GerarMensalidade();
@@ -40,45 +45,57 @@ namespace Academia
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Erro ao tentar se conectar ao banco de dados: {ex.Message}"); 
+                MessageBox.Show($"Erro ao tentar se conectar ao banco de dados: {ex.Message}");
             }
             // Executa sempre
             finally
             {
-                MessageBox.Show("Bem Vindo ao sistema de academia!"); 
+                MessageBox.Show("Bem Vindo ao sistema de academia!");
             }
         }
 
         // Linkando aos formulários
+        private void btnAluno_Click(object sender, EventArgs e)
+        {
+            Navegacao.AbrirUc(alunos);
+                
+            SelecionaBotao(btnAluno);
+        }
+
         private void btnProfessor_Click(object sender, EventArgs e)
         {
             Navegacao.AbrirUc(professores);
+            
+            SelecionaBotao(btnProfessor);
         }
 
-        private void btnModalidades_Click(object sender, EventArgs e)
+        private void btnModalidade_Click(object sender, EventArgs e)
         {
             Navegacao.AbrirUc(modalidades);
+
+            SelecionaBotao(btnModalidade);
         }
 
-        private void btnTurmas_Click(object sender, EventArgs e)
+        private void btnTurma_Click(object sender, EventArgs e)
         {
             Navegacao.AbrirUc(turmas);
+
+            SelecionaBotao(btnTurma);
         }
 
         private void btnCaixa_Click(object sender, EventArgs e)
         {
             caixa ??= new ucCaixa(this);
             Navegacao.AbrirUc(caixa);
+
+            SelecionaBotao(btnCaixa);
         }
 
-        private void btnConfig_Click(object sender, EventArgs e)
+        private void btnConfiguracao_Click(object sender, EventArgs e)
         {
             Navegacao.AbrirUc(configuracoes);
-        }
 
-        private void btnAlunos_Click(object sender, EventArgs e)
-        {
-            Navegacao.AbrirUc(alunos);
+            SelecionaBotao(btnConfiguracao);
         }
 
         // Manipula a visualização do label dependendo da situação do caixa
@@ -97,7 +114,7 @@ namespace Academia
                 else
                 {
                     lblCaixa.Text = "FECHADO";
-                    lblCaixa.ForeColor= Color.Firebrick;
+                    lblCaixa.ForeColor = Color.Firebrick;
                 }
             }
             catch (Exception ex)
@@ -105,5 +122,36 @@ namespace Academia
                 MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        private void SelecionaBotao(Button btnSelecionado)
+        {
+            foreach (var item in icones)
+            {
+                item.Key.ForeColor = SystemColors.ControlDark;
+                item.Key.Image = item.Value.normal;
+            }
+
+            btnSelecionado.ForeColor = SystemColors.ButtonHighlight;
+            btnSelecionado.Image = icones[btnSelecionado].ativo;
+
+            pnlDecoração.Height = btnSelecionado.Height;
+            pnlDecoração.Top = btnSelecionado.Top;
+
+            btnSelecionado.Refresh();
+        }
+
+
+        private void InicializaIcones()
+        {
+            icones = new Dictionary<Button, (Image, Image)>
+            {
+                { btnAluno, (Properties.Resources.alunosIcon_cinza, Properties.Resources.alunosIcon_branco) },
+                { btnProfessor, (Properties.Resources.professoresIcon_cinza, Properties.Resources.professoresIcon_branco) },
+                { btnModalidade, (Properties.Resources.modalidadesIcon_cinza, Properties.Resources.modalidadesIcon_branco) },
+                { btnTurma, (Properties.Resources.turmasIcon_cinza, Properties.Resources.turmasIcon_branco) },
+                { btnCaixa, (Properties.Resources.caixaIcon_cinza, Properties.Resources.caixaIcon_branco) },
+                { btnConfiguracao, (Properties.Resources.configuracaoIcon_cinza, Properties.Resources.configuracaoIcon_branco) }
+            };
+    }
     }
 }
