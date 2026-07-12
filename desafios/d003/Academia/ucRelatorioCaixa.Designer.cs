57961f7a71c679a3258b64909d18b193786efa3e
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-07-11 22:39:16 -0300

    feat: cria o UserControl para relatório do caixa

---
+++ desafios/d003/Academia/ucRelatorioCaixa.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Academia
+{
+    partial class ucRelatorioCaixa
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(926, 617);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // ucRelatorioCaixa
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(flowLayoutPanel1);
+            Name = "ucRelatorioCaixa";
+            Size = new Size(926, 617);
+            Load += ucRelatorioCaixa_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ desafios/d003/Academia/ucRelatorioCaixa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Academia
+{
+    public partial class ucRelatorioCaixa : UserControl
+    {
+        public ucRelatorioCaixa()
+        {
+            InitializeComponent();
+        }
+
+        private readonly Caixa novoCaixa = new();
+
+        private void Carregar()
+        {
+            DataTable caixa = novoCaixa.ListarCaixa();
+
+            foreach (DataRow row in caixa.Rows)
+            {
+                Panel card = new Panel();
+                card.Width = 100;
+                card.Height = 50;
+
+                Label nome = new Label();
+                nome.Text = $"caixa {row["ID_CAIXA"]}";
+
+                card.Controls.Add(nome);
+
+                flowLayoutPanel1.Controls.Add(card);
+            }
+        }
+
+        private void ucRelatorioCaixa_Load(object sender, EventArgs e)
+        {
+            Carregar();
+        }
+    }
+}
