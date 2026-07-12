29c5434a54e0acb9795b0ae5802744fc9efdad6a
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-07-11 23:24:11 -0300

    feat: cria o card para o relatório de caixa

---
+++ desafios/d003/Academia/CardCaixa.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Academia
+{
+    partial class CardCaixa
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lblCaixa = new Label();
+            lblSaldo = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 20);
+            label1.TabIndex = 0;
+            label1.Text = "card";
+            // 
+            // lblCaixa
+            // 
+            lblCaixa.AutoSize = true;
+            lblCaixa.Location = new Point(14, 47);
+            lblCaixa.Name = "lblCaixa";
+            lblCaixa.Size = new Size(22, 20);
+            lblCaixa.TabIndex = 0;
+            lblCaixa.Text = "id";
+            // 
+            // lblSaldo
+            // 
+            lblSaldo.AutoSize = true;
+            lblSaldo.Location = new Point(14, 77);
+            lblSaldo.Name = "lblSaldo";
+            lblSaldo.Size = new Size(45, 20);
+            lblSaldo.TabIndex = 0;
+            lblSaldo.Text = "saldo";
+            // 
+            // CardCaixa
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            Controls.Add(lblSaldo);
+            Controls.Add(lblCaixa);
+            Controls.Add(label1);
+            Name = "CardCaixa";
+            Size = new Size(329, 236);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblCaixa;
+        private Label lblSaldo;
+    }
+}
---
+++ desafios/d003/Academia/CardCaixa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Academia
+{
+    public partial class CardCaixa : UserControl
+    {
+        public CardCaixa()
+        {
+            InitializeComponent();
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public int IdCaixa { get; set; }
+
+        public void Configurar(string nome, decimal saldo)
+        {
+            lblCaixa.Text = nome;
+            lblSaldo.Text = saldo.ToString("C");
+        }
+    }
+}
