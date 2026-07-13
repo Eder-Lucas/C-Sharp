4363cd8d574bd6ceaab7f00f2f5ca83668e79773
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-07-12 23:56:05 -0300

    feat: torna os cards mais responsivos

--- desafios/d003/Academia/ucRelatorioCaixa.cs
+++ desafios/d003/Academia/ucRelatorioCaixa.cs
@@ -24,7 +24,7 @@ namespace Academia
             foreach (DataRow row in caixa.Rows)
             {
                 CardCaixa card = new();
-                card.IdCaixa = Convert.ToInt32(row["ID_CAIXA"]);              
+                card.IdCaixa = Convert.ToInt32(row["ID_CAIXA"]);
 
                 card.Configurar($"Caixa {row["ID_CAIXA"]}", Convert.ToDecimal(row["SALDO_INICIAL"]));
 
@@ -35,6 +35,37 @@ namespace Academia
         private void ucRelatorioCaixa_Load(object sender, EventArgs e)
         {
             Carregar();
+        }
+
+        private void flowLayoutPanel1_SizeChanged(object sender, EventArgs e)
+        {
+            int colunas;
+
+            if(flowLayoutPanel1.Width <= 662)
+            {
+                colunas = 1;
+            }
+            else if (flowLayoutPanel1.Width <= 982)
+            {
+                colunas = 2;
+            }
+            else
+            {
+                colunas = 3;
+            }
+
+            int larguraCard = (flowLayoutPanel1.ClientSize.Width / colunas) - 30;
+
+            // Debug
+            label1.Text = $"Largura do FlowLayoutPanel: {flowLayoutPanel1.ClientSize.Width}, Colunas: {colunas}, Largura do Card: {larguraCard}";
+
+            foreach (Control control in flowLayoutPanel1.Controls)
+            {
+                if (flowLayoutPanel1.Width > 350)
+                    control.Width = larguraCard;
+                else
+                    control.Width = 300;
+            }
         }
     }
 }
