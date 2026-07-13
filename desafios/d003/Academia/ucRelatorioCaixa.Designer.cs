5958e5d2d6b51d6cf40bf2ce2c0b6691aa7b104e
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-07-12 23:56:41 -0300

    feat: adiciona um label para acompanhar o tamanho da tela no debug

--- desafios/d003/Academia/ucRelatorioCaixa.Designer.cs
+++ desafios/d003/Academia/ucRelatorioCaixa.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -36,23 +39,49 @@
             flowLayoutPanel1.AutoScroll = true;
             flowLayoutPanel1.Dock = DockStyle.Fill;
             flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Margin = new Padding(0);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Padding = new Padding(20, 0, 0, 0);
             flowLayoutPanel1.Size = new Size(926, 617);
             flowLayoutPanel1.TabIndex = 0;
+            flowLayoutPanel1.SizeChanged += flowLayoutPanel1_SizeChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(926, 34);
+            panel1.TabIndex = 1;
             // 
             // ucRelatorioCaixa
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
             Controls.Add(flowLayoutPanel1);
             Name = "ucRelatorioCaixa";
             Size = new Size(926, 617);
             Load += ucRelatorioCaixa_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
+        private Panel panel1;
     }
 }
